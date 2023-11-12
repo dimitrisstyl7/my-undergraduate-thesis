718889418a9630919837ff5801b32010d7d026a2
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-11-12 23:20:09 +0200

    TH-6 Web app project created.

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/DietitianHubApplication.java
@@ -0,0 +1,13 @@
+package com.dimstyl.dietitianhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietitianHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietitianHubApplication.class, args);
+    }
+
+}
---
+++ DietitianHub/src/test/java/com/dimstyl/dietitianhub/DietitianHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimstyl.dietitianhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietitianHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
