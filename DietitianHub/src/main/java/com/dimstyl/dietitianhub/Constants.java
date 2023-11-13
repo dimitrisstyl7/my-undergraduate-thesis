a6a96df1cd840d78866043bbf7baab80d73e21ed
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-11-13 12:50:51 +0200

    TH-10 layout.html created successfully.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/Constants.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/Constants.java
@@ -5,8 +5,10 @@ public class Constants {
     public static final String INDEX_URL_1 = "index";
     public static final String INDEX_URL_2 = "";
     public static final String NEW_PAGE_URL = "new-page";
+    public static final String PAGE_LAYOUT_URL = "page-layout";
 
     // Files
     public static final String INDEX_FILE = "index";
     public static final String NEW_PAGE_FILE = "pages-starter";
+    public static final String PAGE_LAYOUT_FILE = "page-layout";
 }
