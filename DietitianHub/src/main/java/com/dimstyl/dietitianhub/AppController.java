d5037264d1a7ce486aad042ff72ac4d4c6fc2557
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-11-13 03:42:51 +0200

    TH-7 dietitian template added.

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/AppController.java
@@ -0,0 +1,19 @@
+package com.dimstyl.dietitianhub;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@NoArgsConstructor
+public class AppController {
+    @GetMapping({Constants.INDEX_URL_1, Constants.INDEX_URL_2})
+    public String indexPage() {
+        return Constants.INDEX_FILE;
+    }
+
+    @GetMapping(Constants.NEW_PAGE_URL)
+    public String newPage() {
+        return Constants.NEW_PAGE_FILE;
+    }
+}
---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/Constants.java
@@ -0,0 +1,12 @@
+package com.dimstyl.dietitianhub;
+
+public class Constants {
+    // URLs
+    public static final String INDEX_URL_1 = "index";
+    public static final String INDEX_URL_2 = "";
+    public static final String NEW_PAGE_URL = "new-page";
+
+    // Files
+    public static final String INDEX_FILE = "index";
+    public static final String NEW_PAGE_FILE = "pages-starter";
+}
