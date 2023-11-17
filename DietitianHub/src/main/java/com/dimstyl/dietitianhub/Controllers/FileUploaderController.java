180fb6e7cb6096d072cfd59322005f0318182861
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-11-17 17:01:02 +0200

    FileUploaderController.java updated.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/Controllers/FileUploaderController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/Controllers/FileUploaderController.java
@@ -2,13 +2,18 @@ package com.dimstyl.dietitianhub.Controllers;
 
 import com.dimstyl.dietitianhub.Constants;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FileUploaderController {
+    @GetMapping(Constants.UPLOAD_FILE_URL)
+    public String uploadFilePage() {
+        return Constants.UPLOAD_FILE_HTML;
+    }
+
     @PostMapping(Constants.UPLOAD_FILE_URL)
-    public String uploadFile() {
+    public void uploadFile() {
         System.out.println("Uploading file...");
-        return Constants.INDEX_HTML;
     }
 }
