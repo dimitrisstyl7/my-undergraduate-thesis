8b6afc20afdd4757eaa184a95008979ac37bf5e7
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-11-17 16:59:40 +0200

    TH-13 "Post announcements" template added.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/Constants.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
     public static final String REMOVE_CLIENT_URL = "remove-client";
     public static final String LOGIN_URL = "login";
     public static final String UPLOAD_FILE_URL = "upload-file";
+    public static final String ANNOUNCEMENTS_URL = "announcements";
 
     // HTML Files
     public static final String INDEX_HTML = "index";
@@ -18,4 +19,5 @@ public class Constants {
     public static final String VIEW_CLIENTS_HTML = "view-clients";
     public static final String LOGIN_HTML = "login";
     public static final String UPLOAD_FILE_HTML = "upload-file";
+    public static final String ANNOUNCEMENTS_HTML = "announcements";
 }
