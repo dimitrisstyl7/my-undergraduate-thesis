92aeb40ec12664c9055d298854a8f94465888100
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-22 20:34:30 +0200

    rename Constants.java to Endpoints.java

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/Endpoints.java
@@ -0,0 +1,25 @@
+package com.dimstyl.dietitianhub;
+
+public class Endpoints {
+    // Endpoints
+    public static final String INDEX_ENDPOINT = "index";
+    public static final String APPOINTMENTS_ENDPOINT = "appointments";
+    public static final String MESSAGES_ENDPOINT = "messages";
+    public static final String REGISTER_CLIENT_ENDPOINT = "register-client";
+    public static final String VIEW_CLIENTS_ENDPOINT = "view-clients";
+    public static final String REMOVE_CLIENT_ENDPOINT = "remove-client";
+    public static final String LOGIN_ENDPOINT = "login";
+    public static final String UPLOAD_FILE_ENDPOINT = "upload-file";
+    public static final String ANNOUNCEMENTS_ENDPOINT = "announcements";
+    public static final String ARTICLES_ENDPOINT = "articles";
+
+    // HTML Files
+    public static final String INDEX_HTML = "index";
+    public static final String APPOINTMENTS_HTML = "appointments";
+    public static final String MESSAGES_HTML = "messages";
+    public static final String VIEW_CLIENTS_HTML = "view-clients";
+    public static final String LOGIN_HTML = "login";
+    public static final String UPLOAD_FILE_HTML = "upload-file";
+    public static final String ANNOUNCEMENTS_HTML = "announcements";
+    public static final String ARTICLES_HTML = "articles";
+}
