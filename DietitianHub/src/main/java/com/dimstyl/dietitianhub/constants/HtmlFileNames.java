d009b090805605c4af4e186f89e90890909495b6
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-03 14:24:07 +0200

    create HtmlFileNames.java into constants package

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/constants/HtmlFileNames.java
@@ -0,0 +1,12 @@
+package com.dimstyl.dietitianhub.constants;
+
+public class HtmlFileNames {
+    public static final String INDEX_HTML = "index";
+    public static final String APPOINTMENTS_HTML = "appointments";
+    public static final String MESSAGES_HTML = "messages";
+    public static final String VIEW_CLIENTS_HTML = "view-clients";
+    public static final String LOGIN_HTML = "login";
+    public static final String UPLOAD_FILE_HTML = "upload-file";
+    public static final String ANNOUNCEMENTS_HTML = "announcements";
+    public static final String ARTICLES_HTML = "articles";
+}
