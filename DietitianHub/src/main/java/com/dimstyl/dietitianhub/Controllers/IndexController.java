46457289f8f4cd478de8cdb5de22284ed3ebdf90
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-03 14:38:59 +0200

    remove @NoArgsConstructor (IndexController.java)

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/Controllers/IndexController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/Controllers/IndexController.java
@@ -1,6 +1,5 @@
 package com.dimstyl.dietitianhub.Controllers;
 
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,7 +7,6 @@ import static com.dimstyl.dietitianhub.constants.Endpoints.INDEX_ENDPOINT;
 import static com.dimstyl.dietitianhub.constants.HtmlFileNames.INDEX_HTML;
 
 @Controller
-@NoArgsConstructor
 public class IndexController {
     @GetMapping({INDEX_ENDPOINT})
     public String indexPage() {
