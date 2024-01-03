cfa7d9d9f06ad9ef2ef9f27566816e88ebf3b43a
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-03 19:21:16 +0200

    update Role entity

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/entity/Role.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/entity/Role.java
@@ -1,15 +1,17 @@
 package com.dimstyl.dietitianhub.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
+@Getter
 @Entity
 @Table(name = "role")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "id", unique = true, nullable = false)
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", unique = true, nullable = false, length = 14)
     private String name;
 }
