875d4a5a53e7945a31d8fe82514e1fc552d5cc0f
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-03 16:19:49 +0200

    update create_tables.sql

--- sql-scripts/create_tables.sql
+++ sql-scripts/create_tables.sql
@@ -1,37 +1,29 @@
-drop table if exists user_role;
-drop table if exists role;
-drop table if exists user_details;
+drop table if exists user_info;
 drop table if exists "user";
+drop table if exists role;
+
+create table role
+(
+    id   serial      not null unique primary key,
+    name varchar(14) not null unique
+-- ROLE_CLIENT, ROLE_DIETITIAN
+);
 
 create table "user"
 (
-    user_id  serial      not null unique primary key,
+    id       serial      not null unique primary key,
     username varchar(50) not null unique,
     password varchar(80) not null,
-    enabled  boolean     not null default true
+    enabled  boolean     not null default true,
+    role_id  int         not null references role (id)
 );
 
 create table user_info
 (
-    user_info_id serial      not null unique primary key,
-    user_id      int         not null unique references "user" (user_id),
-    first_name   varchar(50) not null,
-    last_name    varchar(50) not null,
-    email        varchar(50) not null,
-    phone        varchar(10) not null
-);
-
-create table role
-(
-    role_id serial      not null unique primary key,
-    name    varchar(14) not null unique
--- ROLE_CLIENT, ROLE_DIETITIAN
-);
-
-create table user_role
-(
-    user_id int not null references "user" (user_id),
-    role_id int not null references role (role_id),
-    primary key (user_id)
--- A user can have only one authority (ROLE_CLIENT or ROLE_DIETITIAN)
+    id         serial      not null unique primary key,
+    user_id    int         not null unique references "user" (id),
+    first_name varchar(50) not null,
+    last_name  varchar(50) not null,
+    email      varchar(50) not null,
+    phone      varchar(10) not null
 );
