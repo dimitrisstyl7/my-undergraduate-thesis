ce6ad7fd67d3b6f9c31e760888a917b33f77004d
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-03 16:17:10 +0200

    create Role entity (Role.java)

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/entity/Role.java
@@ -0,0 +1,15 @@
+package com.dimstyl.dietitianhub.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+}
