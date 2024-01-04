245aedcb3e35fbf373a6c9102e29dd296610950e
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-05 01:47:14 +0200

    create insert_data.sql

---
+++ sql-scripts/insert_data.sql
@@ -0,0 +1,14 @@
+insert into role (id, name)
+values (1, 'ROLE_CLIENT'),
+       (2, 'ROLE_DIETITIAN');
+
+-- username: dietitian123, password: dietitian123
+-- username: client123, password: client123
+insert into "user" (id, username, password, role_id)
+values (1, 'dietitian123', '$2a$10$yy.ltCDSA2L1.3KAwmVeAeFxrZwxNf2sfLY3Kue/8bPJ4P9gpk5iG', 2),
+       (2, 'client123', '$2a$10$S91gR8EWTTINPl2dHt/VkOhFWAHZ9jDlI5cJG9tAGc7oaRJJwIIcm', 1);
+
+
+insert into user_info (user_id, first_name, last_name, email, phone)
+values (1, 'Dietitian', 'Dietitian', 'dietitian@email.com', '1234567890'),
+       (2, 'Client', 'Client', 'client@email.com', '0987654321');
