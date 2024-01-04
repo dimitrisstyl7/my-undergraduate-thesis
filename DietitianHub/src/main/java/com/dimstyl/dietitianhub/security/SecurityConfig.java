7950c08307313ee3234169279f628254c849cc99
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-05 01:46:35 +0200

    create SecurityConfig.java

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.dimstyl.dietitianhub.security;
+
+import com.dimstyl.dietitianhub.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(UserService userService) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+}
