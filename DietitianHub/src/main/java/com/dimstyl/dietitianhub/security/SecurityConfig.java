4bf2838162268b4463dce998ac8ef2be1c2cac46
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-05 02:38:06 +0200

    add securityFilterChain method (SecurityConfig.java)

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/security/SecurityConfig.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/security/SecurityConfig.java
@@ -4,7 +4,11 @@ import com.dimstyl.dietitianhub.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static com.dimstyl.dietitianhub.constants.Endpoints.LOGIN_ENDPOINT;
 
 @Configuration
 public class SecurityConfig {
@@ -19,5 +23,21 @@ public class SecurityConfig {
         authenticationProvider.setUserDetailsService(userService);
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(configurer ->
+                        configurer.anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage(LOGIN_ENDPOINT)
+                                .loginProcessingUrl("/authenticate-user")
+                                .permitAll()
+                )
+                .logout(logout -> logout.permitAll())
+                .build();
     }
 }
