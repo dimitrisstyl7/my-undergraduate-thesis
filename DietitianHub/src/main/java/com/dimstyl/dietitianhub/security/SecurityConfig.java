c5b1e06cbc8ec1309a3d72914c2419cf090ddfbd
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-11 01:26:45 +0200

    create customLoginSuccessHandler method (SecurityConfig.java)

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/security/SecurityConfig.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/security/SecurityConfig.java
@@ -1,16 +1,26 @@
 package com.dimstyl.dietitianhub.security;
 
 import com.dimstyl.dietitianhub.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
 
 import static com.dimstyl.dietitianhub.constants.Endpoints.*;
+import static com.dimstyl.dietitianhub.constants.UserRoles.CLIENT_ROLE;
+import static com.dimstyl.dietitianhub.constants.UserRoles.DIETITIAN_ROLE;
 
 @Configuration
 @EnableWebSecurity
@@ -42,11 +52,36 @@ public class SecurityConfig {
                 .formLogin(form ->
                         form
                                 .loginPage(LOGIN_ENDPOINT)
+                                .successHandler(this::customLoginSuccessHandler)
                                 .permitAll()
-                                .loginProcessingUrl("/authenticate-user")
-                                .defaultSuccessUrl(INDEX_ENDPOINT, true)
                 )
                 .logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher(LOGOUT_ENDPOINT)))
                 .build();
+    }
+
+    public void customLoginSuccessHandler(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException {
+        System.out.println("HANDLER EXECUTION");
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            System.out.println("HANDLER EXECUTION 2");
+            System.out.println(authentication.isAuthenticated());
+            return;
+        }
+        System.out.println("HANDLER EXECUTION 3");
+        String role = authentication.getAuthorities().iterator().next().getAuthority();
+        System.out.println(role);
+        switch (role) {
+            case CLIENT_ROLE -> {
+                authentication.setAuthenticated(false);
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+            }
+            case DIETITIAN_ROLE -> {
+                System.out.println("DIETITIAN");
+                response.sendRedirect(INDEX_ENDPOINT);
+            }
+            default -> {
+                // TODO
+            }
+        }
     }
 }
