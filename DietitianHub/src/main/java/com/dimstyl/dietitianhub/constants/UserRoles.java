2b18b53507e09f47ad29448a8967fd6593545150
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-11 01:25:25 +0200

    create UserRoles.java

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/constants/UserRoles.java
@@ -0,0 +1,6 @@
+package com.dimstyl.dietitianhub.constants;
+
+public class UserRoles {
+    public static final String DIETITIAN_ROLE = "DIETITIAN";
+    public static final String CLIENT_ROLE = "CLIENT";
+}
