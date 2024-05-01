137c12f30fa8d4aabd678cdaa0b62ebe1191da3a
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-05-02 00:27:39 +0300

    TH-17 Update ClientsController.java
    - Add RequestMapping annotation.
    - Add RequiredArgsConstructor annotation.
    - Working on viewClientsPage method (/clients endpoint).

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/ClientsController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/ClientsController.java
@@ -1,12 +1,21 @@
 package com.dimstyl.dietitianhub.controllers;
 
+import com.dimstyl.dietitianhub.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/clients")
+@RequiredArgsConstructor
 public class ClientsController {
-    @GetMapping("/clients")
-    public String viewClientsPage() {
-        return "view-clients";
+    private final UserService userService;
+
+    @GetMapping
+    public String viewClientsPage(Model model) {
+        model.addAttribute("clients", userService.getAllClients());
+        return "clients/view-clients";
     }
 }
