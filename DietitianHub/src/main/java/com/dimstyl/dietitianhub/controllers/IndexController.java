3f63cf0790500c11e2eda02643186d426a5901d9
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-05-01 21:55:25 +0300

    Update IndexController.java

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/IndexController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/IndexController.java
@@ -8,7 +8,7 @@ import static com.dimstyl.dietitianhub.constants.HtmlFiles.INDEX_HTML;
 
 @Controller
 public class IndexController {
-    @GetMapping({INDEX_ENDPOINT})
+    @GetMapping(INDEX_ENDPOINT)
     public String indexPage() {
         return INDEX_HTML;
     }
