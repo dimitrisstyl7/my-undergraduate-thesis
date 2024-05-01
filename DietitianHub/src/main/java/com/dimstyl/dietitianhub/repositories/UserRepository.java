b18fbde14500da3cf5ca7434ca587dce321cdf57
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-05-02 00:24:06 +0300

    TH-17 Add findAllByRole_Name method in repositories\UserRepository.java

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/repositories/UserRepository.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/repositories/UserRepository.java
@@ -3,6 +3,10 @@ package com.dimstyl.dietitianhub.repositories;
 import com.dimstyl.dietitianhub.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    List<User> findAllByRole_Name(String role_name);
 }
