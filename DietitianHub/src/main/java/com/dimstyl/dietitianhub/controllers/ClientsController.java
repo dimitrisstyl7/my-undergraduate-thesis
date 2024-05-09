31c5810753c9d519c2466da61c2d96a275742346
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-05-10 00:49:41 +0300

    TH-16 Update controllers\ClientsController.java
    - Add "/register" endpoint for client registration.
    - Implement the appropriate code for client registration logic.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/ClientsController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/ClientsController.java
@@ -1,10 +1,15 @@
 package com.dimstyl.dietitianhub.controllers;
 
+import com.dimstyl.dietitianhub.dtos.ClientDto;
 import com.dimstyl.dietitianhub.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,6 +21,21 @@ public class ClientsController {
     @GetMapping
     public String viewClientsPage(Model model) {
         model.addAttribute("clients", userService.getAllClients());
+        model.addAttribute("newClient", new ClientDto());
         return "clients/view-clients";
+    }
+
+    @PostMapping("/register")
+    public String registerClient(
+            @Valid @ModelAttribute("newClient") ClientDto clientDto,
+            BindingResult result,
+            Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("clients", userService.getAllClients());
+            model.addAttribute("validationFailed", true);
+            return "clients/view-clients";
+        }
+        userService.registerClient(clientDto);
+        return "redirect:/clients";
     }
 }
