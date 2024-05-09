d61c71d3f6cda019396b79621231b5bd75340224
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-05-10 00:38:16 +0300

    TH-16 Create services\UserInfoService.java
    - Create saveUserInfo method.

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/services/UserInfoService.java
@@ -0,0 +1,7 @@
+package com.dimstyl.dietitianhub.services;
+
+import com.dimstyl.dietitianhub.entities.UserInfo;
+
+public interface UserInfoService {
+    public void saveUserInfo(UserInfo userInfo);
+}
