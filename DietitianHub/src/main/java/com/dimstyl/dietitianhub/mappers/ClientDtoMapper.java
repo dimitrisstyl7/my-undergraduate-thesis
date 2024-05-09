da9c12339292820a5def07945655ea5632cd096b
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-05-10 00:42:04 +0300

    TH-16 Create mappers\ClientDtoMapper.java
    - Create mapToUserOfRoleClient and mapToUserInfo methods.

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/mappers/ClientDtoMapper.java
@@ -0,0 +1,38 @@
+package com.dimstyl.dietitianhub.mappers;
+
+import com.dimstyl.dietitianhub.dtos.ClientDto;
+import com.dimstyl.dietitianhub.entities.Role;
+import com.dimstyl.dietitianhub.entities.User;
+import com.dimstyl.dietitianhub.entities.UserInfo;
+
+import static com.dimstyl.dietitianhub.enums.UserRole.CLIENT;
+import static com.dimstyl.dietitianhub.utilities.RegistrationUtil.generateUsername;
+
+public class ClientDtoMapper {
+    public static User mapToUserOfRoleClient(ClientDto clientDto) {
+        return User.builder()
+                .username(generateUsername(
+                        clientDto.getFirstName(),
+                        clientDto.getLastName(),
+                        clientDto.getDateOfBirth()))
+                /* Todo: For new clients, the password will by empty.
+                    A registration email will be sent to the client
+                    with a link to set a password.
+                 */
+                .password("")
+                .role(new Role(CLIENT.getId(), CLIENT.getRole()))
+                .build();
+    }
+
+    public static UserInfo mapToUserInfo(ClientDto clientDto, User user) {
+        return UserInfo.builder()
+                .user(user)
+                .firstName(clientDto.getFirstName())
+                .lastName(clientDto.getLastName())
+                .gender(clientDto.getGender())
+                .dateOfBirth(clientDto.getDateOfBirth())
+                .email(clientDto.getEmail())
+                .phone(clientDto.getPhone())
+                .build();
+    }
+}
