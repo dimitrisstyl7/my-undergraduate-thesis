37863489b3dfaab57e8f154d4b29dd518e5fb32c
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-05-10 00:25:57 +0300

    Update dtos\ClientDto.java
    - Update annotation validations.
    - Create getFormattedDateOfBirth method (used in resources\templates\clients\view-clients.html).

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/ClientDto.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/ClientDto.java
@@ -1,5 +1,6 @@
 package com.dimstyl.dietitianhub.dtos;
 
+import com.dimstyl.dietitianhub.enums.Month;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
@@ -11,25 +12,36 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClientDto {
-    @NotBlank
-    @Size(min = 1, max = 50)
+    @NotBlank(message = "First Name cannot be empty.")
+    @Size(min = 2, max = 50, message = "First Name must be between 2 and 50 characters.")
+    @Pattern(regexp = "^[a-zA-Z]{2,50}$", message = "First Name must contain only letters.")
     private String firstName;
 
-    @NotBlank
-    @Size(min = 1, max = 50)
+    @NotBlank(message = "Last Name cannot be empty.")
+    @Size(min = 2, max = 50, message = "Last Name must be between 2 and 50 characters.")
+    @Pattern(regexp = "^[a-zA-Z]{2,50}$", message = "Last Name must contain only letters.")
     private String lastName;
 
     private char gender;
 
-    @NotNull
-    @Past
-    private Date dateOfBirth;
+    @NotNull(message = "Date of birth is required.")
+    @Past(message = "Date of birth must be in the past.")
+    private LocalDate dateOfBirth;
 
-    @Email
-    @Size(min = 1, max = 50)
+    @Email(message = "Invalid email address.")
+    @Size(min = 3, max = 50, message = "Email must be between 3 and 50 characters.")
     private String email;
 
-    @NotBlank
-    @Size(min = 1, max = 20)
+    @NotBlank(message = "Phone number cannot be empty.")
+    @Size(min = 8, max = 20, message = "Phone number must be between 8 and 20 digits.")
+    @Pattern(regexp = "^[0-9]{8,20}$", message = "Phone number must contain only digits.")
     private String phone;
+
+    public String getFormattedDateOfBirth() {
+        // Return the date of birth in the format "day month year".
+        String day = String.valueOf(dateOfBirth.getDayOfMonth());
+        String month = Month.byNumber(dateOfBirth.getMonthValue());
+        String year = String.valueOf(dateOfBirth.getYear());
+        return day + " " + month + " " + year;
+    }
 }
