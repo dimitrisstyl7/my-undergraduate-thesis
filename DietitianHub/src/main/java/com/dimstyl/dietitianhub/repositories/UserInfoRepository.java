dbe27352869b838d025a847ad2e7c7220c2268b2
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-05-24 18:01:09 +0300

    Update UserInfoRepository.java
    - Add findByUser method.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/repositories/UserInfoRepository.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/repositories/UserInfoRepository.java
@@ -1,7 +1,9 @@
 package com.dimstyl.dietitianhub.repositories;
 
+import com.dimstyl.dietitianhub.entities.User;
 import com.dimstyl.dietitianhub.entities.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+    UserInfo findByUser(User user);
 }
