2f8425646de3be64eaa5d4a7c3ff0a132aee00d7
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-05-31 17:16:38 +0300

    TH-59 Create dtos\TagCategoryDto.java and dtos\TagDto.java

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/TagCategoryDto.java
@@ -0,0 +1,15 @@
+package com.dimstyl.dietitianhub.dtos;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+public class TagCategoryDto {
+    private String name;
+    private List<TagDto> tags;
+}
---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/TagDto.java
@@ -0,0 +1,14 @@
+package com.dimstyl.dietitianhub.dtos;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class TagDto {
+    Integer id;
+    Integer categoryId;
+    String name;
+}
