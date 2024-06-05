ae606d425957ba197d77680feb917176917f376f
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-06-05 16:49:58 +0300

    TH-47 Update exceptionHandlers\ErrorResponse.java
    - Add redirectUrl argument.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/exceptionHandlers/ErrorResponse.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/exceptionHandlers/ErrorResponse.java
@@ -2,6 +2,5 @@ package com.dimstyl.dietitianhub.exceptionHandlers;
 
 import java.time.LocalDateTime;
 
-public record ErrorResponse(LocalDateTime timestamp, int statusCode, String message) {
-
+public record ErrorResponse(LocalDateTime timestamp, int statusCode, String message, String redirectUrl) {
 }
