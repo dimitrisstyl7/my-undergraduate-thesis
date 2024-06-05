f4766506fe5e835b2c18b9323c3b19f96d943534
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-06-05 17:03:06 +0300

    TH-47 Update exceptionHandlers\RestResponseEntityExceptionHandler.java
    - Remove inheritance (extends ResponseEntityExceptionHandler).
    - Rename and update handleNotFoundException method to handleUserNotFoundException.
    - Rename and update handleTagMismatchException method to handleTagsMismatchException.
    - Create handleTagCategoriesNotFoundException method.
    - Create handleBadRequestException method.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/exceptionHandlers/RestResponseEntityExceptionHandler.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/exceptionHandlers/RestResponseEntityExceptionHandler.java
@@ -1,36 +1,70 @@
 package com.dimstyl.dietitianhub.exceptionHandlers;
 
-import com.dimstyl.dietitianhub.customExceptions.ApiTagMismatchException;
+import com.dimstyl.dietitianhub.customExceptions.ApiTagCategoriesNotFoundException;
+import com.dimstyl.dietitianhub.customExceptions.ApiTagsMismatchException;
 import com.dimstyl.dietitianhub.customExceptions.ApiUserInfoNotFoundException;
 import com.dimstyl.dietitianhub.customExceptions.ApiUserNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
-public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {ApiUserNotFoundException.class, ApiUserInfoNotFoundException.class})
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    protected ErrorResponse handleNotFoundException() {
+    protected ErrorResponse handleUserNotFoundException() {
         return new ErrorResponse(
                 LocalDateTime.now(),
                 HttpStatus.NOT_FOUND.value(),
-                HttpStatus.NOT_FOUND.getReasonPhrase()
+                "The operation could not be completed because the user" +
+                        "was not found. Please try again. If the problem persists," +
+                        "please contact our Support.",
+                "/error/userNotFound"
         );
     }
 
-    @ExceptionHandler(value = ApiTagMismatchException.class)
+    @ExceptionHandler(value = ApiTagsMismatchException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    protected ErrorResponse handleTagMismatchException() {
+    protected ErrorResponse handleTagsMismatchException() {
         return new ErrorResponse(
                 LocalDateTime.now(),
                 HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase()
+                "Something went wrong while processing the tags." +
+                        " Please try again. If the problem persists," +
+                        "please contact our Support.",
+                "/error/tagsMismatch"
+        );
+    }
+
+    @ExceptionHandler(value = ApiTagCategoriesNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    protected ErrorResponse handleTagCategoriesNotFoundException() {
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "The operation could not be completed because no tags were found." +
+                        " Please try again. If the problem persists," +
+                        "please contact our Support.",
+                "/error/tagsNotFound"
+        );
+    }
+
+    @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class, HttpMessageNotReadableException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    protected ErrorResponse handleBadRequestException() {
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "The operation could not be completed because the request was invalid." +
+                        " Please try again. If the problem persists," +
+                        "please contact our Support.",
+                "/error/400"
         );
     }
 
