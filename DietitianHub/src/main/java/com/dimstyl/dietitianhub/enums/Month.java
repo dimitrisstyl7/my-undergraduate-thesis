e5450763fcdf5d12bad62c7255fe7eda2365cbc5
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-06-13 22:45:55 +0300

    Refactor lines in Month.java

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/enums/Month.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/enums/Month.java
@@ -5,7 +5,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum Month {
 
-    Jan(1), Feb(2), Mar(3), Apr(4), May(5), Jun(6), Jul(7), Aug(8), Sep(9), Oct(10), Nov(11), Dec(12);
+    Jan(1), Feb(2), Mar(3), Apr(4), May(5), Jun(6),
+    Jul(7), Aug(8), Sep(9), Oct(10), Nov(11), Dec(12);
 
     private final int monthNumber;
 
