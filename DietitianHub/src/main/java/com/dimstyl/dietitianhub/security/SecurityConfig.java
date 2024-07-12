42c790d147237657b74772314c38b4670fffa117
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-12 19:04:03 +0300

    Update SecurityConfig.java
    - Update the return type of passwordEncoder method.
    - Add a LoginPageFilter in securityFilterChain.
    - Update logout configuration in securityFilterChain.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/security/SecurityConfig.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/security/SecurityConfig.java
@@ -6,7 +6,9 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static com.dimstyl.dietitianhub.enums.UserRole.DIETITIAN;
@@ -16,7 +18,7 @@ import static com.dimstyl.dietitianhub.enums.UserRole.DIETITIAN;
 public class SecurityConfig {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -31,25 +33,24 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(configurer ->
-                        configurer
-                                .requestMatchers(
-                                        "/css/**", "/data/**", "/fonts/**",
-                                        "/images/**", "/js/**", "/libs/**"
-                                ).permitAll()
-                                .anyRequest().hasAuthority(DIETITIAN.getRole())
+                .addFilterBefore(new LoginPageFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers(
+                                "/css/**", "/data/**", "/fonts/**",
+                                "/images/**", "/js/**", "/libs/**"
+                        ).permitAll()
+                        .anyRequest().hasAuthority(DIETITIAN.getRole())
                 )
-                .formLogin(form ->
-                        form
-                                .loginPage("/auth/login")
-                                .loginProcessingUrl("/auth/authenticate-user")
-                                .failureUrl("/auth/login?error")
-                                .permitAll()
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/auth/authenticate-user")
+                        .failureUrl("/auth/login?error")
+                        .permitAll()
                 )
                 .logout(logout -> logout
-                        .logoutUrl("/auth/logout")
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"))
-                        .logoutSuccessUrl("/auth/login?logout").permitAll()
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"))
+                                .logoutSuccessUrl("/auth/login?logout")
+                                .permitAll()
                 )
                 .build();
     }
