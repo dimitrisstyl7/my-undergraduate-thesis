2f2dc2538828a4df8674ce5b324a1a6d60c06627
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-27 22:19:39 +0300

    Update CustomUserDetailsService.java

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/security/CustomUserDetailsService.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/security/CustomUserDetailsService.java
@@ -4,6 +4,7 @@ import com.dimstyl.dietitianhub.entities.Role;
 import com.dimstyl.dietitianhub.entities.User;
 import com.dimstyl.dietitianhub.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -15,6 +16,8 @@ import org.springframework.stereotype.Service;
 import java.util.Collection;
 import java.util.stream.Stream;
 
+import static com.dimstyl.dietitianhub.enums.UserRole.CLIENT;
+
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
@@ -25,10 +28,20 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
 
+        // Check if user exists
         if (user == null) {
             throw new UsernameNotFoundException("User with username %s not found.".formatted(username));
         }
 
+        // TODO: May I handle differently the following checks?
+
+        // Check if user has unauthorized role
+        String role = user.getRole().getName();
+        if (role.equals(CLIENT.getRole())) {
+            throw new AccessDeniedException("User with username %s has unauthorized role (%s).".formatted(username, role));
+        }
+
+        // Check if user is enabled
         if (!user.isEnabled()) {
             throw new DisabledException("User with username %s is disabled.".formatted(username));
         }
