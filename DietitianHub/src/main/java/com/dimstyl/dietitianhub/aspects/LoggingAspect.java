e4452bae29360a4ad167fcb764aec7f444a48a07
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-28 20:40:15 +0300

    Update LoggingAspect.java

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/aspects/LoggingAspect.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/aspects/LoggingAspect.java
@@ -23,7 +23,15 @@ public class LoggingAspect {
     @AfterThrowing(pointcut = "serviceMethods() || userDetailsServiceMethod()", throwing = "ex")
     public void logException(JoinPoint joinPoint, Throwable ex) throws Throwable {
         String methodName = joinPoint.getSignature().toLongString();
+
+        // Log the exception message
         log.error("Exception thrown in method: {}, exception message: {}", methodName, ex.getMessage());
+
+        // Check if the exception has a cause and log it
+        Throwable cause = ex.getCause();
+        if (cause != null) log.error("Cause of the exception: {}", cause.getMessage());
+
+        // Re-throw the exception to maintain the original behavior
         throw ex;
     }
 
