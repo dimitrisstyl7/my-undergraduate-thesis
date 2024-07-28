ebef446c2dc3383537d4a422e07f0e1f187ca96a
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-28 21:01:25 +0300

    Update MvcErrorController.java
    - Update and add endpoints for available error pages.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcErrorController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcErrorController.java
@@ -8,6 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/error")
 public class MvcErrorController {
 
+    @GetMapping("/400")
+    public String BadRequestPage() {
+        return "error/400";
+    }
+
+    @GetMapping("/403")
+    public String ForbiddenPage() {
+        return "error/403";
+    }
+
+    @GetMapping("/404")
+    public String NotFoundPage() {
+        return "error/404";
+    }
+
+    @GetMapping("/500")
+    public String InternalServerErrorPage() {
+        return "error/500";
+    }
+
     @GetMapping("/userNotFound")
     public String UserNotFoundPage() {
         return "error/user-not-found";
@@ -23,9 +43,19 @@ public class MvcErrorController {
         return "error/tags-not-found";
     }
 
-    @GetMapping("/400")
-    public String BadRequestPage() {
-        return "error/400";
+    @GetMapping("/dietPlanNotFound")
+    public String DietPlanNotFoundPage() {
+        return "error/diet-plan-not-found";
+    }
+
+    @GetMapping("/dietPlanUploadFailed")
+    public String DietPlanUploadFailedPage() {
+        return "error/diet-plan-upload-failed";
+    }
+
+    @GetMapping("/dietPlanDeleteFailed")
+    public String DietPlanDeleteFailedPage() {
+        return "error/diet-plan-delete-failed";
     }
 
 }
