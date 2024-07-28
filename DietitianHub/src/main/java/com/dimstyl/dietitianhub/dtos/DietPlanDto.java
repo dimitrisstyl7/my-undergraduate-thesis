ee200112a0e4cd55b0041c462c1c680a5857a08c
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-28 21:00:42 +0300

    TH-19 Create DietPlanDto.java

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/DietPlanDto.java
@@ -0,0 +1,17 @@
+package com.dimstyl.dietitianhub.dtos;
+
+import com.dimstyl.dietitianhub.enums.Month;
+
+import java.time.LocalDate;
+
+public record DietPlanDto(int id, String title, LocalDate createdOn) {
+
+    public String getFormattedDate() {
+        // Return the date in the format "day month year"
+        String day = String.valueOf(createdOn.getDayOfMonth());
+        String month = Month.byNumber(createdOn.getMonthValue());
+        String year = String.valueOf(createdOn.getYear());
+        return "%s %s %s".formatted(day, month, year);
+    }
+
+}
