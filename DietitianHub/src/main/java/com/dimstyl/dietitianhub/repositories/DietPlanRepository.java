fbe7d34543844bce8049391d1d9ead9ca001b9c5
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-28 21:03:29 +0300

    TH-19 Create DietPlanRepository.java
    - Implement `findByIdAndUserInfo_Id` to retrieve a diet plan by ID and user information ID.
    - Implement `findAllByUserInfo_IdOrderByCreatedOnDesc` to retrieve all diet plans for a user, ordered by creation date in descending order.
    - Implement `existsByUserInfo_IdAndCreatedOn` to check if a diet plan exists for a user on a specific date.

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/repositories/DietPlanRepository.java
@@ -0,0 +1,18 @@
+package com.dimstyl.dietitianhub.repositories;
+
+import com.dimstyl.dietitianhub.entities.DietPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface DietPlanRepository extends JpaRepository<DietPlan, Integer> {
+
+    Optional<DietPlan> findByIdAndUserInfo_Id(int dietPlanId, int userInfoId);
+
+    List<DietPlan> findAllByUserInfo_IdOrderByCreatedOnDesc(int userInfoId);
+
+    boolean existsByUserInfo_IdAndCreatedOn(int userInfoId, LocalDate createdOn);
+
+}
