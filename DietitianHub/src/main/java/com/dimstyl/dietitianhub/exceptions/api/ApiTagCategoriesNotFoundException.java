fc71558da0f4d2c0a7fc501071a8d41b9f186421
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-29 18:31:23 +0300

    Delete ApiTagCategoriesNotFoundException.java

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/exceptions/api/ApiTagCategoriesNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.dimstyl.dietitianhub.exceptions.api;
-
-public class ApiTagCategoriesNotFoundException extends RuntimeException {
-
-    public ApiTagCategoriesNotFoundException(String message) {
-        super(message);
-    }
-
-}
