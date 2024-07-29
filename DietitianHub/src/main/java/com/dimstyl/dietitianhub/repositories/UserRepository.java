05a5477d7f8f40e8d2d8b9830fdff17233971e80
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-29 18:34:56 +0300

    Update UserRepository.java
    - Change the return type of the findByUsername method.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/repositories/UserRepository.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/repositories/UserRepository.java
@@ -4,10 +4,11 @@ import com.dimstyl.dietitianhub.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
     List<User> findAllByRole_NameAndEnabledIsTrue(String role_name);
 
--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/security/CustomUserDetailsService.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/security/CustomUserDetailsService.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static com.dimstyl.dietitianhub.enums.UserRole.CLIENT;
@@ -26,12 +27,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
+        Optional<User> optionalUser = userRepository.findByUsername(username);
 
         // Check if user exists
-        if (user == null) {
-            throw new UsernameNotFoundException("User with username %s not found.".formatted(username));
-        }
+        User user = optionalUser.orElseThrow(() ->
+                new UsernameNotFoundException("User with username %s not found.".formatted(username)));
 
         // TODO: May I handle differently the following checks?
 
