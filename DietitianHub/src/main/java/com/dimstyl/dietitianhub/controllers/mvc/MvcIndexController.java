433600538318394df309410eedc9e24b2be7d77a
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-29 18:09:41 +0300

    Update MvcIndexController.java

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcIndexController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcIndexController.java
@@ -1,12 +1,14 @@
 package com.dimstyl.dietitianhub.controllers.mvc;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MvcIndexController {
 
-    @GetMapping("/index")
+    @GetMapping({"/", "/index"})
     public String indexPage() {
         return "index";
     }
