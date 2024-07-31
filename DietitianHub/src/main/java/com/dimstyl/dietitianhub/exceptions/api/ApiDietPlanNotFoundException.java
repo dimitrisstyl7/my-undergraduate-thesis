eb2fc9b33943d69c8fcad4098bfc2b17ec90a595
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-31 13:31:54 +0300

    Delete ApiDietPlanNotFoundException.java

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/exceptions/api/ApiDietPlanNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.dimstyl.dietitianhub.exceptions.api;
-
-public class ApiDietPlanNotFoundException extends RuntimeException {
-
-    public ApiDietPlanNotFoundException(String message) {
-    }
-
-}
