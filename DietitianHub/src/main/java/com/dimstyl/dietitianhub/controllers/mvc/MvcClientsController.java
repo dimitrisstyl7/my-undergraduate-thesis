db47c8ce322b5dae5f15cdba6d091502e92bf39f
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-31 14:27:14 +0300

    TH-19 Update MvcClientsController.java
    - Add uploadDietPlanPage and uploadDietPlan methods:
      - Introduce `uploadDietPlanPage` to show the diet plan upload form with existing diet plans.
      - Introduce `uploadDietPlan` method to handle diet plan file uploads.
      - Implement validation for file type and presence, redirecting with appropriate error or success messages.
    
    - Add downloadDietPlan and viewDietPlan methods:
      - Introduce `downloadDietPlan` method to handle downloading of diet plans as attachments.
      - Introduce `viewDietPlan` method to handle viewing diet plans inline in the browser.
    
    - Add deleteDietPlan method:
      - Introduce a method to delete a diet plan with the endpoint `/delete`.
      - Redirect with success message after successful deletion of the diet plan.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcClientsController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcClientsController.java
@@ -1,17 +1,26 @@
 package com.dimstyl.dietitianhub.controllers.mvc;
 
 import com.dimstyl.dietitianhub.dtos.ClientDto;
+import com.dimstyl.dietitianhub.dtos.DietPlanDto;
 import com.dimstyl.dietitianhub.entities.User;
+import com.dimstyl.dietitianhub.services.DietPlanService;
 import com.dimstyl.dietitianhub.services.TagCategoryService;
 import com.dimstyl.dietitianhub.services.UserInfoService;
 import com.dimstyl.dietitianhub.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -22,6 +31,7 @@ public class MvcClientsController {
     private final UserService userService;
     private final UserInfoService userInfoService;
     private final TagCategoryService tagCategoryService;
+    private final DietPlanService dietPlanService;
 
     @GetMapping
     public String viewClientsPage(@RequestParam("success") Optional<String> success,
@@ -81,6 +91,68 @@ public class MvcClientsController {
         userService.disableUser(id);
         redirectAttributes.addFlashAttribute("flashAttribute", "Client deleted successfully.");
         return "redirect:/clients?success";
+    }
+
+    @GetMapping("/{id}/dietPlans/upload")
+    public String uploadDietPlanPage(@PathVariable("id") int id, Model model) {
+        User user = userService.getUserById(id);
+        List<DietPlanDto> dietPlans = dietPlanService.getDietPlansByUserInfoId(user.getUserInfo().getId());
+        model.addAttribute("clientId", id);
+        model.addAttribute("dietPlans", dietPlans);
+        return "upload-diet-plan";
+    }
+
+    @PostMapping("/{id}/dietPlans/upload")
+    public String uploadDietPlan(@PathVariable("id") int id, @RequestParam("file") MultipartFile file) {
+        // Redirect with error message if no file is selected
+        if (file.isEmpty()) {
+            return "redirect:/clients/%d/dietPlans/upload?noFile".formatted(id);
+        }
+
+        // Redirect with error message if the file is not a PDF
+        String fileContentType = file.getContentType();
+        if (fileContentType == null || !fileContentType.equals("application/pdf")) {
+            return "redirect:/clients/%d/dietPlans/upload?invalidFile".formatted(id);
+        }
+
+        // Save the diet plan file
+        User user = userService.getUserById(id);
+        dietPlanService.saveDietPlan(user.getUserInfo(), file);
+
+        // Redirect with success message
+        return "redirect:/clients/%d/dietPlans/upload?uploadSuccess".formatted(id);
+    }
+
+    @GetMapping("/{clientId}/dietPlans/{dietPlanId}/download")
+    public ResponseEntity<Resource> downloadDietPlan(@PathVariable("clientId") int clientId,
+                                                     @PathVariable("dietPlanId") int dietPlanId) {
+        User user = userService.getUserById(clientId);
+        Resource file = dietPlanService.getDietPlanFileAsResource(dietPlanId, user.getUserInfo());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + file.getFilename() + "\"")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(file);
+    }
+
+    @GetMapping("/{clientId}/dietPlans/{dietPlanId}/view")
+    public ResponseEntity<Resource> viewDietPlan(@PathVariable("clientId") int clientId,
+                                                 @PathVariable("dietPlanId") int dietPlanId) {
+        User user = userService.getUserById(clientId);
+        Resource file = dietPlanService.getDietPlanFileAsResource(dietPlanId, user.getUserInfo());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "inline; filename=\"" + file.getFilename() + "\"")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(file);
+    }
+
+    @GetMapping("/{clientId}/dietPlans/{dietPlanId}/delete")
+    public String deleteDietPlan(@PathVariable("clientId") int clientId,
+                                 @PathVariable("dietPlanId") int dietPlanId) {
+        User user = userService.getUserById(clientId);
+        dietPlanService.deleteDietPlan(dietPlanId, user.getUserInfo());
+        return "redirect:/clients/%d/dietPlans/upload?deleteSuccess".formatted(clientId);
     }
 
 }
