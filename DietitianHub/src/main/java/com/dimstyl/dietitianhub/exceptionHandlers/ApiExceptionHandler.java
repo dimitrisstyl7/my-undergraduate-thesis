6cc1f462aadd93ac7b7f684565db7de1cd2a1d90
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-31 14:16:18 +0300

    Update ApiExceptionHandler.java
    - Rename handleUserNotFoundException:
      - Replace `UserNotFoundException` with `ApiUserInfoNotFoundException` to better reflect the exception's purpose.
      - Update error message formatting for clarity.
    
    - Simplify exception handling:
      - Remove handling for `ApiTagCategoriesNotFoundException`, as it is no longer necessary.
      - Remove handling for `MethodArgumentTypeMismatchException` and `HttpMessageNotReadableException`, as they are no longer needed.
    
    - Update handleTagsMismatchException:
      - Rename `ApiTagsMismatchException` to `TagsMismatchException` for consistency.
      - Enhance error message formatting for improved readability.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/exceptionHandlers/ApiExceptionHandler.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/exceptionHandlers/ApiExceptionHandler.java
@@ -1,71 +1,40 @@
 package com.dimstyl.dietitianhub.exceptionHandlers;
 
-import com.dimstyl.dietitianhub.exceptions.api.ApiTagCategoriesNotFoundException;
-import com.dimstyl.dietitianhub.exceptions.api.ApiTagsMismatchException;
-import com.dimstyl.dietitianhub.exceptions.api.ApiUserInfoNotFoundException;
-import com.dimstyl.dietitianhub.exceptions.api.ApiUserNotFoundException;
+import com.dimstyl.dietitianhub.exceptions.ApiUserInfoNotFoundException;
+import com.dimstyl.dietitianhub.exceptions.TagsMismatchException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
+    @ExceptionHandler(ApiUserInfoNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    protected ErrorResponse handleUserNotFoundException() {
+    protected ErrorResponse handleUserInfoNotFoundException() {
         return new ErrorResponse(
                 LocalDateTime.now(),
                 HttpStatus.NOT_FOUND.value(),
-                "The operation could not be completed because the user" +
-                        "was not found. Please try again. If the problem persists," +
-                        "please contact our Support.",
+                """
+                        The operation could not be completed because the user was not found. \
+                        Please try again. If the problem persists, please contact our Support.""",
                 "/error/userNotFound"
         );
     }
 
-    @ExceptionHandler(value = ApiTagsMismatchException.class)
+    @ExceptionHandler(TagsMismatchException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     protected ErrorResponse handleTagsMismatchException() {
         return new ErrorResponse(
                 LocalDateTime.now(),
                 HttpStatus.BAD_REQUEST.value(),
-                "Something went wrong while processing the tags." +
-                        " Please try again. If the problem persists," +
-                        "please contact our Support.",
+                """
+                        Something went wrong while processing the tags. Please try again. \
+                        If the problem persists, please contact our Support.""",
                 "/error/tagsMismatch"
-        );
-    }
-
-    @ExceptionHandler(value = ApiTagCategoriesNotFoundException.class)
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    protected ErrorResponse handleTagCategoriesNotFoundException() {
-        return new ErrorResponse(
-                LocalDateTime.now(),
-                HttpStatus.NOT_FOUND.value(),
-                """
-                         The operation could not be completed because no tags were found.\
-                         Please try again. If the problem persists,\
-                        please contact our Support.""",
-                "/error/tagsNotFound"
-        );
-    }
-
-    @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class, HttpMessageNotReadableException.class})
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    protected ErrorResponse handleBadRequestException() {
-        return new ErrorResponse(
-                LocalDateTime.now(),
-                HttpStatus.BAD_REQUEST.value(),
-                """
-                        The operation could not be completed because the request was invalid.\
-                         Please try again. If the problem persists,\
-                        please contact our Support.""",
-                "/error/400"
         );
     }
 
