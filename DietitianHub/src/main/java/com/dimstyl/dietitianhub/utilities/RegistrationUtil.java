2057346d0fee0456903f30930dbd9029a2f02eae
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-08 18:49:49 +0300

    TH-51 Enhance RegistrationUtil with password generation functionality
    + Add `generatePassword` method:
      - Introduce password generation functionality to create secure, random passwords.
      - Combine numbers, special characters, and alphabets for a robust password.
    
    + Implement random character generation:
      - Implement helper methods to generate random alphabets, numbers, and special characters.
      - Use `SecureRandom` for better randomness and security.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/utilities/RegistrationUtil.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/utilities/RegistrationUtil.java
@@ -2,10 +2,19 @@ package com.dimstyl.dietitianhub.utilities;
 
 import lombok.experimental.UtilityClass;
 
+import java.security.SecureRandom;
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @UtilityClass
 public final class RegistrationUtil {
+
+    private final static Random random = new SecureRandom();
 
     public static String generateUsername(String firstName, String lastName, LocalDate dateOfBirth) {
         /*
@@ -19,6 +28,46 @@ public final class RegistrationUtil {
         String ln = lastName.substring(0, 1).toUpperCase() + lastName.substring(1).toLowerCase();
         String dob = String.valueOf(dateOfBirth.getYear()).substring(2);
         return fn + ln + dob;
+    }
+
+    public static String generatePassword() {
+        Stream<Character> pwdStream = Stream.concat(
+                getRandomNumbers(3),
+                Stream.concat(
+                        getRandomSpecialChars(3),
+                        Stream.concat(
+                                getRandomAlphabets(2, true),
+                                getRandomAlphabets(4, false)
+                        )
+                )
+        );
+
+        List<Character> charList = pwdStream.collect(Collectors.toList());
+        Collections.shuffle(charList);
+
+        return charList.stream()
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
+    }
+
+    private static Stream<Character> getRandomAlphabets(int count, boolean upperCase) {
+        IntStream characters;
+        if (upperCase) {
+            characters = random.ints(count, 65, 90);
+        } else {
+            characters = random.ints(count, 97, 122);
+        }
+        return characters.mapToObj(data -> (char) data);
+    }
+
+    public static Stream<Character> getRandomNumbers(int count) {
+        IntStream numbers = random.ints(count, 48, 57);
+        return numbers.mapToObj(data -> (char) data);
+    }
+
+    private static Stream<Character> getRandomSpecialChars(int count) {
+        IntStream specialChars = random.ints(count, 33, 45);
+        return specialChars.mapToObj(data -> (char) data);
     }
 
 }
