1306f7cc60ac90cdaab14cc3804874d24daa4a73
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-08 19:04:28 +0300

    TH-51  Create ClientCredentialChangeDto.java
    + Add validation to ClientCredentialChangeDto for client credential updates.
    
    + Implement DTO validation:
      - Added `@PasswordMatches` annotation to `ClientCredentialChangeDto` to ensure password and confirmation fields match.
      - Applied constraints to `username` field.
      - Added constraints to `password` field.

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/ClientCredentialChangeDto.java
@@ -0,0 +1,39 @@
+package com.dimstyl.dietitianhub.dtos;
+
+import com.dimstyl.dietitianhub.validators.password.PasswordMatches;
+import com.dimstyl.dietitianhub.validators.username.Unique;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@PasswordMatches
+public class ClientCredentialChangeDto {
+
+
+    @NotBlank(message = "Username cannot be empty.")
+    @Size(min = 8, max = 30, message = "Username must be between 8 and 30 characters.")
+    @Pattern(
+            regexp = "^[A-Za-z][A-Za-z0-9_]{7,29}$",
+            message = "Username must start with a letter and contain only letters, digits, and underscores."
+    )
+    @Unique
+    private String username;
+
+    @NotBlank(message = "Password cannot be empty.")
+    @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters.")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=!.<>])(?!.* ).{8,64}$",
+            message = "Password must contain at least one letter, one digit, one special character (@#$%^&+=!.<>), and no spaces."
+    )
+    private String password;
+
+    @NotBlank(message = "Confirm password cannot be empty.")
+    private String confirmPassword;
+
+}
