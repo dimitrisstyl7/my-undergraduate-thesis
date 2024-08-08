e79bc63ae4b51eb44a242eb5637fe87547f1d8f6
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-08 18:46:56 +0300

    TH-51 Create RegistrationFailedException

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/exceptions/RegistrationFailedException.java
@@ -0,0 +1,9 @@
+package com.dimstyl.dietitianhub.exceptions;
+
+public class RegistrationFailedException extends RuntimeException {
+
+    public RegistrationFailedException(String message) {
+        super(message);
+    }
+
+}
