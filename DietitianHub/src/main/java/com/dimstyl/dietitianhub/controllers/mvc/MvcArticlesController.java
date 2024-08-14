8ab5d2fbc76357060dc211d2f63daaa02e4ae917
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-15 00:48:46 +0300

    TH-73 Update MvcArticlesController.java
    + Update `MvcArticlesController` to Include Services:
      - Add `ArticleService` and `TagCategoryService` as dependencies using `@RequiredArgsConstructor`.
      - Update `articlesPage()` method:
        - Add `articles` attribute to the model with data from `articleService.getAllArticles()`.
        - Add `tagCategories` attribute to the model with data from `tagCategoryService.getAllTagCategoriesAndTags()`.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcArticlesController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcArticlesController.java
@@ -1,13 +1,25 @@
 package com.dimstyl.dietitianhub.controllers.mvc;
 
+import com.dimstyl.dietitianhub.services.ArticleService;
+import com.dimstyl.dietitianhub.services.TagCategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/articles")
+@RequiredArgsConstructor
 public class MvcArticlesController {
 
-    @GetMapping("/articles")
-    public String articlesPage() {
+    private final TagCategoryService tagCategoryService;
+    private final ArticleService articleService;
+
+    @GetMapping
+    public String articlesPage(Model model) {
+        model.addAttribute("articles", articleService.getAllArticles());
+        model.addAttribute("tagCategories", tagCategoryService.getAllTagCategoriesAndTags());
         return "articles";
     }
 
