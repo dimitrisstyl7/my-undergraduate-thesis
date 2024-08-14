81b3fd4fdd4d9415263f257dbdde0d009c4f267a
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-15 00:06:57 +0300

    Update ClientDto.java

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/ClientDto.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/ClientDto.java
@@ -16,30 +16,30 @@ public class ClientDto {
 
     private int id;
 
-    @NotBlank(message = "First Name cannot be empty.")
-    @Size(min = 2, max = 50, message = "First Name must be between 2 and 50 characters.")
-    @Pattern(regexp = "^[a-zA-Z]{2,50}$", message = "First Name must contain only letters.")
+    @NotBlank(message = "First Name cannot be empty")
+    @Size(min = 2, max = 50, message = "First Name must be between 2 and 50 characters")
+    @Pattern(regexp = "^[a-zA-Z]{2,50}$", message = "First Name must contain only letters")
     private String firstName;
 
-    @NotBlank(message = "Last Name cannot be empty.")
-    @Size(min = 2, max = 50, message = "Last Name must be between 2 and 50 characters.")
-    @Pattern(regexp = "^[a-zA-Z]{2,50}$", message = "Last Name must contain only letters.")
+    @NotBlank(message = "Last Name cannot be empty")
+    @Size(min = 2, max = 50, message = "Last Name must be between 2 and 50 characters")
+    @Pattern(regexp = "^[a-zA-Z]{2,50}$", message = "Last Name must contain only letters")
     private String lastName;
 
-    @GenderConstraint(message = "Gender is required.")
+    @GenderConstraint(message = "Gender is required")
     private char gender;
 
-    @NotNull(message = "Date of birth is required.")
-    @Past(message = "Date of birth must be in the past.")
+    @NotNull(message = "Date of birth is required")
+    @Past(message = "Date of birth must be in the past")
     private LocalDate dateOfBirth;
 
     @Email(message = "Invalid email address.")
-    @Size(min = 3, max = 50, message = "Email must be between 3 and 50 characters.")
+    @Size(min = 3, max = 50, message = "Email must be between 3 and 50 characters")
     private String email;
 
-    @NotBlank(message = "Phone number cannot be empty.")
-    @Size(min = 8, max = 20, message = "Phone number must be between 8 and 20 digits.")
-    @Pattern(regexp = "^[0-9]{8,20}$", message = "Phone number must contain only digits.")
+    @NotBlank(message = "Phone number cannot be empty")
+    @Size(min = 8, max = 20, message = "Phone number must be between 8 and 20 digits")
+    @Pattern(regexp = "^[0-9]{8,20}$", message = "Phone number must contain only digits")
     private String phone;
 
     public String getFormattedDateOfBirth() {
