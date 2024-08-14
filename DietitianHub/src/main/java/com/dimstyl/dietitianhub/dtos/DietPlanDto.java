1a72c9b4b704d56472a8a18adfc5ffe0eb84f82d
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-15 00:13:02 +0300

    Update DietPlanDto.java
    + Replace `LocalDate` with `String` for `createdOn` field:
      - Accept date as a pre-formatted `String` instead of handling date formatting within the DTO.
      - Ensure that any date formatting is performed outside this class, simplifying its structure.
    
    + Remove `getFormattedDate()` method:
      - No longer required since `createdOn` is now provided as a formatted string.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/DietPlanDto.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/DietPlanDto.java
@@ -1,17 +1,4 @@
 package com.dimstyl.dietitianhub.dtos;
 
-import com.dimstyl.dietitianhub.enums.Month;
-
-import java.time.LocalDate;
-
-public record DietPlanDto(int id, String name, LocalDate createdOn) {
-
-    public String getFormattedDate() {
-        // Return the date in the format "day month year"
-        String day = String.valueOf(createdOn.getDayOfMonth());
-        String month = Month.byNumber(createdOn.getMonthValue());
-        String year = String.valueOf(createdOn.getYear());
-        return "%s %s %s".formatted(day, month, year);
-    }
-
+public record DietPlanDto(int id, String name, String createdOn) {
 }
