60e06dc236012b70225810a2c45cb1e806f717ef
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-15 00:49:54 +0300

    TH-73 Create ApiArticlesController.java
    + Add `ApiArticlesController` for API Endpoints:
      - Implement `getArticle(int id)` to return an `ArticleDto` for a given article ID.
      - Implement `createArticle(ArticleDto articleDto, BindingResult result)`:
        - Validate `ArticleDto` and create a new article if no errors are found.
        - Return validation errors if any are present.
      - Implement `updateArticle(int id, ArticleDto articleDto, BindingResult result)`:
        - Validate `ArticleDto` and update the existing article if no errors are found.
        - Return validation errors if any are present.
      - Implement `deleteArticle(int id)` to delete an article by ID.

---
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/api/ApiArticlesController.java
@@ -0,0 +1,84 @@
+package com.dimstyl.dietitianhub.controllers.api;
+
+import com.dimstyl.dietitianhub.dtos.ArticleDto;
+import com.dimstyl.dietitianhub.services.ArticleService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/articles")
+@RequiredArgsConstructor
+public class ApiArticlesController {
+
+    private final ArticleService articleService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ArticleDto> getArticle(@PathVariable("id") int id) {
+        return ResponseEntity.ok(articleService.getArticle(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, String>> createArticle(@Valid @RequestBody ArticleDto articleDto,
+                                                             BindingResult result) {
+        // If there are no errors, continue with the creation of the article.
+        if (!result.hasErrors()) {
+            articleService.createArticle(articleDto);
+            return ResponseEntity.noContent().location(URI.create("/articles?publishSuccess")).build();
+        }
+
+        // If there are errors but no default message is found, return a bad request without a body.
+        // Otherwise, return a bad request without a body.
+        var titleError = result.getFieldError("title");
+        var contentError = result.getFieldError("content");
+        Map<String, String> errors = new HashMap<>() {
+            {
+                if (titleError != null
+                    && titleError.getDefaultMessage() != null) put("title", titleError.getDefaultMessage());
+
+                if (contentError != null
+                    && contentError.getDefaultMessage() != null) put("content", contentError.getDefaultMessage());
+            }
+        };
+        return errors.isEmpty() ? ResponseEntity.badRequest().build() : ResponseEntity.badRequest().body(errors);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String, String>> updateArticle(@PathVariable("id") int id,
+                                                             @Valid @RequestBody ArticleDto articleDto,
+                                                             BindingResult result) {
+        // If there are no errors, continue with the update of the article.
+        if (!result.hasErrors()) {
+            articleService.updateArticle(id, articleDto);
+            return ResponseEntity.noContent().location(URI.create("/articles?updateSuccess")).build();
+        }
+
+        // If there are errors but no default message is found, return a bad request without a body.
+        // Otherwise, return a bad request without a body.
+        var titleError = result.getFieldError("title");
+        var contentError = result.getFieldError("content");
+        Map<String, String> errors = new HashMap<>() {
+            {
+                if (titleError != null
+                    && titleError.getDefaultMessage() != null) put("title", titleError.getDefaultMessage());
+
+                if (contentError != null
+                    && contentError.getDefaultMessage() != null) put("content", contentError.getDefaultMessage());
+            }
+        };
+        return errors.isEmpty() ? ResponseEntity.badRequest().build() : ResponseEntity.badRequest().body(errors);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable("id") int id) {
+        articleService.deleteArticle(id);
+        return ResponseEntity.noContent().location(URI.create("/articles?deleteSuccess")).build();
+    }
+
+}
