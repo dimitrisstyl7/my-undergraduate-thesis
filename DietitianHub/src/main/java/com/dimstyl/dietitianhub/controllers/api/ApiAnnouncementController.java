b056bb7160cc2236f13859f51667560103f4a02d
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-18 23:54:18 +0300

    TH-27 Add update endpoint for announcements in ApiAnnouncementController
    - Implement PUT /api/v1/announcements/{id} to update existing announcements.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/api/ApiAnnouncementController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/api/ApiAnnouncementController.java
@@ -2,12 +2,15 @@ package com.dimstyl.dietitianhub.controllers.api;
 
 import com.dimstyl.dietitianhub.dtos.AnnouncementDto;
 import com.dimstyl.dietitianhub.services.AnnouncementService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/announcements")
@@ -19,6 +22,34 @@ public class ApiAnnouncementController {
     @GetMapping("/{id}")
     public ResponseEntity<AnnouncementDto> getAnnouncement(@PathVariable("id") int id) {
         return ResponseEntity.ok(announcementService.getAnnouncement(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String, String>> updateAnnouncement(@PathVariable("id") int id,
+                                                                  @Valid @RequestBody AnnouncementDto announcementDto,
+                                                                  BindingResult result) {
+        // If there are no errors, continue with the update of the announcement.
+        if (!result.hasErrors()) {
+            announcementService.updateAnnouncement(id, announcementDto);
+            return ResponseEntity.noContent().location(URI.create("/announcements?updateSuccess")).build();
+        }
+
+        // Otherwise, return a bad request.
+        var titleError = result.getGlobalError();
+        var contentError = result.getFieldError("content");
+        Map<String, String> errors = new HashMap<>() {
+            {
+                if (titleError != null
+                    && titleError.getDefaultMessage() != null) put("title", titleError.getDefaultMessage());
+
+                if (contentError != null
+                    && contentError.getDefaultMessage() != null) put("content", contentError.getDefaultMessage());
+            }
+        };
+
+        // If errors map is empty (no default message found), return a bad request without a body.
+        // Otherwise, return a bad request with the errors map in the body.
+        return errors.isEmpty() ? ResponseEntity.badRequest().build() : ResponseEntity.badRequest().body(errors);
     }
 
 }
