82fe02f391eea6e3ced888c3acc8d029fdc64f8c
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-18 12:00:20 +0300

    TH-26 Enhance MvcAnnouncementsController to use AnnouncementService for data
    - Add `AnnouncementService` dependency to `MvcAnnouncementsController`.
    - Populate the model with today's, yesterday's, and earlier announcements.
    - Return the "announcements" view with the populated model attributes.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcAnnouncementsController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcAnnouncementsController.java
@@ -1,13 +1,32 @@
 package com.dimstyl.dietitianhub.controllers.mvc;
 
+import com.dimstyl.dietitianhub.dtos.AnnouncementDto;
+import com.dimstyl.dietitianhub.services.AnnouncementService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/announcements")
+@RequiredArgsConstructor
 public class MvcAnnouncementsController {
 
-    @GetMapping("/announcements")
-    public String announcementsPage() {
+    private final AnnouncementService announcementService;
+
+    @GetMapping
+    public String announcementsPage(Model model) {
+        List<AnnouncementDto> announcementsForToday = announcementService.getAnnouncementsForToday();
+        List<AnnouncementDto> announcementsForYesterday = announcementService.getAnnouncementsForYesterday();
+        List<AnnouncementDto> earlierAnnouncements = announcementService.getEarlierAnnouncements();
+
+        model.addAttribute("announcementsForToday", announcementsForToday);
+        model.addAttribute("announcementsForYesterday", announcementsForYesterday);
+        model.addAttribute("earlierAnnouncements", earlierAnnouncements);
+
         return "announcements";
     }
 
