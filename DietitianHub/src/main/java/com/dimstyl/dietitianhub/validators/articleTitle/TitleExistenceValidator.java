981ae332c05b3dd2b027ed19735ef9c62b71a463
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-18 11:41:36 +0300

    Refactor articleTitle\TitleExistenceValidator to handle POST and PUT methods
    - Introduce HttpServletRequest to differentiate between POST (create) and PUT (update) requests.
    - Ensure that title uniqueness validation applies only to new titles during updates (PUT), while enforcing strict uniqueness during article creation (POST).
    - Improve flexibility in title validation based on request type.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/validators/articleTitle/TitleExistenceValidator.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/validators/articleTitle/TitleExistenceValidator.java
@@ -2,6 +2,7 @@ package com.dimstyl.dietitianhub.validators.articleTitle;
 
 import com.dimstyl.dietitianhub.dtos.ArticleDto;
 import com.dimstyl.dietitianhub.services.ArticleService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 public class TitleExistenceValidator implements ConstraintValidator<UniqueTitle, ArticleDto> {
 
     private final ArticleService articleService;
+    private final HttpServletRequest request;
 
     @Override
     public void initialize(UniqueTitle constraintAnnotation) {
@@ -18,8 +20,17 @@ public class TitleExistenceValidator implements ConstraintValidator<UniqueTitle,
 
     @Override
     public boolean isValid(ArticleDto articleDto, ConstraintValidatorContext constraintValidatorContext) {
-        String oldTitle = articleService.getArticle(articleDto.id()).title();
         String newTitle = articleDto.title();
+
+        // If the request method is POST (creating a new article), check if the title already exists.
+        if (request.getMethod().equals("POST")) {
+            return !articleService.existsByTitle(newTitle);
+        }
+
+        // If the request method is PUT (updating an existing article),
+        // allow the old title to be the same as the new title.
+        // Otherwise, check if the new title already exists.
+        String oldTitle = articleService.getArticle(articleDto.id()).title();
         return oldTitle.equals(newTitle) || !articleService.existsByTitle(newTitle);
     }
 
