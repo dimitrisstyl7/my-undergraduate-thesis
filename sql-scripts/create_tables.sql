fe96c58986c042a2ac60033d249550fd7a2ba99c
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-20 17:48:44 +0300

    Refactor database schema by replacing the role table with an enum type for user roles.
    - Drop the 'role' table and create a PostgreSQL enum type (role_enum) to represent user roles.
    - Update the 'user' table to use the new 'role_enum' type instead of a foreign key to the 'role' table.
    - Modify data insertion scripts to use the new enum values directly, eliminating the need to insert roles separately.

--- sql-scripts/create_tables.sql
+++ sql-scripts/create_tables.sql
@@ -7,13 +7,9 @@ drop table if exists tag;
 drop table if exists tag_category;
 drop table if exists user_info;
 drop table if exists "user";
-drop table if exists role;
+drop type if exists role_enum;
 
-create table role
-(
-    id   serial     not null unique primary key,
-    name varchar(9) not null unique -- CLIENT, DIETITIAN
-);
+create type role_enum as enum ('CLIENT', 'DIETITIAN');
 
 create table "user"
 (
@@ -21,7 +17,7 @@ create table "user"
     username varchar(102) not null unique,
     password varchar(60)  not null,
     enabled  boolean      not null default false,
-    role_id  int          not null references role (id)
+    role     role_enum    not null
 );
 
 create table user_info
--- sql-scripts/insert_data.sql
+++ sql-scripts/insert_data.sql
@@ -1,12 +1,8 @@
-insert into role (name)
-values ('CLIENT'),
-       ('DIETITIAN');
-
 -- DIETITIAN -> username: aliceWhite95, password: aliceWhite95
 -- CLIENT -> username: johnBrown90, password: johnBrown90
-insert into "user" (username, password, enabled, role_id)
-values ('aliceWhite95', '$2a$10$6cZ5L3DqviiLHWV.8ci8leDRKd9nk1nv0xAz/B.Qm2/E6.uQEmaHO', true, 2),
-       ('johnBrown90', '$2a$10$d/./JcM59U/3vh4eDy/.9uRb7zA.tJmD267s17BJufVjXN0ZfkKDa', true, 1);
+insert into "user" (username, password, enabled, role)
+values ('aliceWhite95', '$2a$10$6cZ5L3DqviiLHWV.8ci8leDRKd9nk1nv0xAz/B.Qm2/E6.uQEmaHO', true, 'DIETITIAN'),
+       ('johnBrown90', '$2a$10$d/./JcM59U/3vh4eDy/.9uRb7zA.tJmD267s17BJufVjXN0ZfkKDa', true, 'CLIENT');
 
 insert into user_info (user_id, first_name, last_name, gender, date_of_birth, email, phone)
 values (1, 'Alice', 'White', 'F', '1995-06-16', 'aliceWhite95@email.com', '1234567890'),
