fa4e884cce697d8f495d821b7ae1b0cf2378ad54
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-24 00:28:22 +0300

    TH-31 Add validation annotations to AppointmentDto
    - Introduce `@NotBlank` and `@Size` annotations to the `title` field in `AppointmentDto`.
    - Ensure the title is not empty and limits its length to 100 characters.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/AppointmentDto.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/AppointmentDto.java
@@ -1,7 +1,16 @@
 package com.dimstyl.dietitianhub.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDateTime;
 
-public record AppointmentDto(int id, String title, String description, LocalDateTime start,
-                             String formattedScheduledDateTime, String clientFullName) {
+public record AppointmentDto(int id,
+                             @NotBlank(message = "Title cannot be empty")
+                             @Size(max = 100, message = "Title must be less than 100 characters")
+                             String title,
+                             String description,
+                             LocalDateTime start,
+                             String formattedScheduledDateTime,
+                             String clientFullName) {
 }
