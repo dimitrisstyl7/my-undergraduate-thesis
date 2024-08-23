9b376fb1e33048a34249403f697d8fc6c809d062
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-24 00:30:17 +0300

    TH-31 Add update functionality to ApiAppointmentController
    - Introduce `updateAppointment` method to handle PUT requests for updating appointments.
    - Validate incoming AppointmentDto using `@Valid` and `BindingResult`.
    - Return a bad request response if validation errors are found, specifically checking for title errors.
    - Utilize `ValidationErrorUtil` to extract title validation errors and return them in the response body.
    - Proceed with updating the appointment if no validation errors are present.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/api/ApiAppointmentController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/api/ApiAppointmentController.java
@@ -3,13 +3,17 @@ package com.dimstyl.dietitianhub.controllers.api;
 import com.dimstyl.dietitianhub.dtos.AppointmentDto;
 import com.dimstyl.dietitianhub.enums.AppointmentStatus;
 import com.dimstyl.dietitianhub.services.AppointmentService;
+import com.dimstyl.dietitianhub.utilities.ValidationErrorUtil;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/v1/appointments")
@@ -21,6 +25,26 @@ public class ApiAppointmentController {
     @GetMapping
     public ResponseEntity<List<AppointmentDto>> getAppointments() {
         return ResponseEntity.ok(appointmentService.getAppointmentsByStatus(AppointmentStatus.SCHEDULED));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String, String>> updateAppointment(@PathVariable("id") int id,
+                                                                 @Valid @RequestBody AppointmentDto appointmentDto,
+                                                                 BindingResult result) {
+        // If there is an error in title, return a bad request.
+        if (result.hasErrors()) {
+            Optional<String> titleError = ValidationErrorUtil.getTitleError(result);
+
+            // If default message is found, return a bad request with the title error in the body.
+            // Otherwise, return a bad request without a body.
+            return titleError
+                    .map(s -> ResponseEntity.badRequest().body(Map.of("title", s)))
+                    .orElseGet(() -> ResponseEntity.badRequest().build());
+        }
+
+        // Otherwise, continue with the update of the appointment.
+        appointmentService.updateAppointment(id, appointmentDto);
+        return ResponseEntity.ok().build();
     }
 
 }
