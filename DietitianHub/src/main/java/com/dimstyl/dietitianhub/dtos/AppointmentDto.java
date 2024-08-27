82a0359e432bcecc9b6d18af95a0a8bbbe39fe31
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-27 11:51:42 +0300

    TH-29 Enhance `AppointmentDto` with validation and entity conversion method
    - Add `@Unique` annotation to validate unique appointment scheduling.
    - Include `@Future` and `@NotNull` annotations for `start` to ensure future dates and non-null values.
    - Add `@NotNull` and `@Range` annotations for `clientId` to ensure valid client selection.
    - Implement `toAppointment` method to convert `AppointmentDto` to `Appointment` entity.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/AppointmentDto.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/dtos/AppointmentDto.java
@@ -1,8 +1,14 @@
 package com.dimstyl.dietitianhub.dtos;
 
+import com.dimstyl.dietitianhub.entities.Appointment;
+import com.dimstyl.dietitianhub.entities.UserInfo;
+import com.dimstyl.dietitianhub.validators.dateTime.Unique;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
+import org.hibernate.validator.constraints.Range;
 
 import java.time.LocalDateTime;
 
@@ -12,7 +18,23 @@ public record AppointmentDto(int id,
                              @Size(max = 100, message = "Title must be less than 100 characters")
                              String title,
                              String description,
+                             @NotNull(message = "Date and Time cannot be empty")
+                             @Future(message = "Date and Time must be in the future")
+                             @Unique
                              LocalDateTime start,
                              String formattedScheduledDateTime,
+                             @NotNull(message = "Please select a client")
+                             @Range(min = 1, message = "Please select a client")
+                             Integer clientId,
                              String clientFullName) {
+
+    public Appointment toAppointment(UserInfo userInfo) {
+        return Appointment.builder()
+                .title(title)
+                .description(description)
+                .scheduledDateTime(start)
+                .clientUserInfo(userInfo)
+                .build();
+    }
+
 }
