dd1bd5984291c18edb052d6a4b1306efcd2ccb03
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-27 12:23:53 +0300

    TH-29 Enhance calendar initialization with create and edit modals
    - Add functionality to open the create modal on date click and set the default datetime.
    - Include reset and error handling for the edit modal.
    - Attach event handlers for save and submit buttons in both edit and create modals.
    - Ensure proper clearing of error messages when modals are closed.
    - Refactor FullCalendar instance creation and event handling for better organization.

--- DietitianHub/src/main/resources/static/js/calendar.init.js
+++ DietitianHub/src/main/resources/static/js/calendar.init.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
-    new FullCalendar.Calendar($('#calendar')[0], {
+    const editModal = $("#edit-modal");
+    const createModal = $("#create-modal");
+
+    // Create a new FullCalendar instance.
+    const calendar = new FullCalendar.Calendar($('#calendar')[0], {
         themeSystem: "bootstrap",
         headerToolbar: {
             left: "prev,next today",
@@ -18,22 +22,47 @@ $(document).ready(function () {
         eventClassNames: "custom-event",
         events: () => fetchAppointments(),
         eventClick: function (info) {
-            const editModal = $("#edit-modal");
+            const eventTitle = info.event.title;
+            const eventDescription = info.event.extendedProps.description;
 
             // Set form values.
-            $("#edit-title").val(info.event.title);
-            $("#edit-description").val(info.event.extendedProps.description);
+            $("#edit-title").val(eventTitle);
+            $("#edit-description").val(eventDescription);
             $("#edit-datetime").val(info.event.extendedProps.formattedScheduledDateTime);
             $("#edit-client-fullname").val(info.event.extendedProps.clientFullName);
 
             // Attach click event handler for the save button.
             $("#edit-save-button").off("click").on("click", () => updateAppointment(info.event));
 
-            // Show the modal.
-            editModal.modal("toggle");
+            // Attach click event handler for the delete button.
 
-            // Always unbind and rebind the hidden event handler.
-            editModal.off('hidden.bs.modal').on('hidden.bs.modal', () => resetEditModal());
+            // Show edit modal.
+            editModal.modal("toggle");
         },
-    }).render();
+        dateClick: function (info) {
+            // Set the date to the selected date, and an arbitrary time.
+            $("#create-datetime").val(`${info.dateStr}T12:00`);
+
+            // Show create modal.
+            createModal.modal("toggle");
+        }
+    });
+
+    // Attach click event handler for the edit reset button.
+    $("#edit-reset-button").off("click").on("click", () => resetEditModal());
+
+    // On edit modal close, clear any error messages.
+    editModal.off("hidden.bs.modal").on("hidden.bs.modal", () => clearEditErrorMessages());
+
+    // On create modal close, reset the form and clear any error messages.
+    createModal.off("hidden.bs.modal").on("hidden.bs.modal", () => {
+        $("#create-form")[0].reset();
+        clearCreateErrorMessages();
+    });
+
+    // Set on click event handler for the create submit button.
+    $("#create-submit-button").off("click").on("click", () => createAppointment(calendar));
+
+    // Render the calendar.
+    calendar.render();
 });
