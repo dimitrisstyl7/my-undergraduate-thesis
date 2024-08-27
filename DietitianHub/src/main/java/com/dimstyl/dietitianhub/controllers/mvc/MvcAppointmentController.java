eb62e51d889f4324d4886c578b2b1753fd8c5323
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-27 12:09:12 +0300

    TH-29 Update MvcAppointmentController
    - Update `MvcAppointmentController` to include `@RequestMapping` for a common path prefix.
    - Inject `UserService` into `MvcAppointmentController` to fetch and display all clients on the appointments page.
    - Add `minDateTime` attribute to the model with the current date and time for restricting the `datetime-local` input field.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcAppointmentController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcAppointmentController.java
@@ -1,13 +1,26 @@
 package com.dimstyl.dietitianhub.controllers.mvc;
 
+import com.dimstyl.dietitianhub.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
+@RequestMapping("/appointments")
+@RequiredArgsConstructor
 public class MvcAppointmentController {
 
-    @GetMapping("/appointments")
-    public String appointmentsPage() {
+    private final UserService userService;
+
+    @GetMapping
+    public String appointmentsPage(Model model) {
+
+        model.addAttribute("clients", userService.getAllClients());
+        model.addAttribute("minDateTime", LocalDateTime.now().toString().substring(0, 16));
         return "appointments";
     }
 
