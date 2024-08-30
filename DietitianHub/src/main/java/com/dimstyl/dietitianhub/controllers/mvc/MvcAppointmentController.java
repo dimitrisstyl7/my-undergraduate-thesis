6307d3509686bb1fc1dba32068009b85ffa3ca5d
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-30 21:41:27 +0300

    Improve MvcAppointmentController for Enhanced Appointment Management
    - Inject `AppointmentService` to manage appointments.
    - Update `appointmentsPage` to:
      - Display today's appointments with status `SCHEDULED` and `COMPLETED`.
      - Display pending appointments sorted by scheduled date and time.
      - Pass `dateTimeNow` and updated list of clients to the model.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcAppointmentController.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/controllers/mvc/MvcAppointmentController.java
@@ -1,5 +1,9 @@
 package com.dimstyl.dietitianhub.controllers.mvc;
 
+import com.dimstyl.dietitianhub.dtos.AppointmentDto;
+import com.dimstyl.dietitianhub.dtos.ClientDto;
+import com.dimstyl.dietitianhub.enums.AppointmentStatus;
+import com.dimstyl.dietitianhub.services.AppointmentService;
 import com.dimstyl.dietitianhub.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,7 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 @RequestMapping("/appointments")
@@ -15,12 +21,31 @@ import java.time.LocalDateTime;
 public class MvcAppointmentController {
 
     private final UserService userService;
+    private final AppointmentService appointmentService;
 
     @GetMapping
     public String appointmentsPage(Model model) {
+        LocalDateTime dateTimeNow = LocalDateTime.now();
+        String minDateTime = dateTimeNow.toString().substring(0, 16);
+        List<ClientDto> clients = userService.getAllClients();
+        List<AppointmentDto> todaysAppointments =
+                appointmentService.getAppointmentsByStatusAndScheduledDateTimeIsBetween(
+                        List.of(AppointmentStatus.SCHEDULED, AppointmentStatus.COMPLETED),
+                        LocalDate.now().atStartOfDay(),
+                        LocalDate.now().atStartOfDay().plusHours(23).plusMinutes(59)
+                );
+        List<AppointmentDto> pendingAppointments =
+                appointmentService.getAppointmentsByStatusAndScheduledDateTimeIsAfterOrderByScheduledDateTime(
+                        AppointmentStatus.PENDING,
+                        dateTimeNow
+                );
 
-        model.addAttribute("clients", userService.getAllClients());
-        model.addAttribute("minDateTime", LocalDateTime.now().toString().substring(0, 16));
+        model.addAttribute("clients", clients);
+        model.addAttribute("minDateTime", minDateTime);
+        model.addAttribute("dateTimeNow", dateTimeNow);
+        model.addAttribute("todaysAppointments", todaysAppointments);
+        model.addAttribute("pendingAppointments", pendingAppointments);
+
         return "appointments";
     }
 
