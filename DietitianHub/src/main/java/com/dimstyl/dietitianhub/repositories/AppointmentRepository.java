c68db44f34de650051ac3790dcc3c244f7ff2c89
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-08-30 21:22:39 +0300

    Enhance AppointmentRepository with additional query methods
    - Add method `findAllByStatusInAndScheduledDateTimeIsAfter` to retrieve appointments with multiple statuses after a specific datetime.
    - Introduce `findAllByStatusAndScheduledDateTimeIsAfterOrderByScheduledDateTime` to fetch appointments by status and after a specific datetime, ordered by `ScheduledDateTime`.
    - Add `findAllByStatusAndScheduledDateTimeIsBefore` for retrieving appointments by status and before a specified datetime.
    - Implement `findAllByStatusInAndScheduledDateTimeIsBetweenOrderByScheduledDateTime` to fetch appointments by status and within a date range, ordered by `ScheduledDateTime`.
    - Add methods to fetch a limited number of records, such as `findFirst5ByStatusAndScheduledDateTimeIsAfterOrderByScheduledDateTime` and `findFirst5ByStatusOrderByScheduledDateTimeDesc`.
    - Introduce `findByScheduledDateTimeAndStatusAndClientUserInfoId` to retrieve specific appointments based on datetime, status, and client ID.
    - Modify `existsByScheduledDateTime` to `existsByScheduledDateTimeAndStatus` for more accurate existence checks.

--- DietitianHub/src/main/java/com/dimstyl/dietitianhub/repositories/AppointmentRepository.java
+++ DietitianHub/src/main/java/com/dimstyl/dietitianhub/repositories/AppointmentRepository.java
@@ -6,12 +6,31 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
 
-    List<Appointment> findAllByStatusAndScheduledDateTimeIsAfter(AppointmentStatus status,
-                                                                 LocalDateTime scheduledDateTime);
+    List<Appointment> findAllByStatusInAndScheduledDateTimeIsAfter(List<AppointmentStatus> statuses,
+                                                                   LocalDateTime dateTime);
 
-    boolean existsByScheduledDateTime(LocalDateTime scheduledDateTime);
+    List<Appointment> findAllByStatusAndScheduledDateTimeIsAfterOrderByScheduledDateTime(AppointmentStatus status,
+                                                                                         LocalDateTime dateTime);
+
+    List<Appointment> findAllByStatusAndScheduledDateTimeIsBefore(AppointmentStatus status, LocalDateTime dateTime);
+
+    List<Appointment> findAllByStatusInAndScheduledDateTimeIsBetweenOrderByScheduledDateTime(List<AppointmentStatus> statuses,
+                                                                                             LocalDateTime startDateTime,
+                                                                                             LocalDateTime endDateTime);
+
+    List<Appointment> findFirst5ByStatusAndScheduledDateTimeIsAfterOrderByScheduledDateTime(AppointmentStatus status,
+                                                                                            LocalDateTime dateTime);
+
+    List<Appointment> findFirst5ByStatusOrderByScheduledDateTimeDesc(AppointmentStatus status);
+
+    Optional<Appointment> findByScheduledDateTimeAndStatusAndClientUserInfoId(LocalDateTime dateTime,
+                                                                              AppointmentStatus status,
+                                                                              int clientUserInfoId);
+
+    boolean existsByScheduledDateTimeAndStatus(LocalDateTime dateTime, AppointmentStatus status);
 
 }
