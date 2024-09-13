1e0ba78ad6527e1dd3569d9e983cf94221802773
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-13 15:02:06 +0300

    TH-76 Enhance MainScreen with ModalNavigationDrawer, TopBar, BottomBar, and Logout Dialog (DietitianHub-Android)
    - Add a `ModalNavigationDrawer` to support side navigation with drawer state management.
    - Introduce a `Scaffold` structure including `TopBar` and `BottomBar`, enabling dynamic navigation and title updates.
    - Integrate an `AlertDialog` for handling logout confirmation within the `MainScreen`.
    - Utilize `MainViewModel` to manage the screen state and handle navigation actions.

--- DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/screens/main/MainScreen.kt
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/screens/main/MainScreen.kt
@@ -1,5 +1,78 @@
 package gr.unipi.thesis.dimstyl.ui.screens.main
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import gr.unipi.thesis.dimstyl.ui.components.AlertDialog
+import gr.unipi.thesis.dimstyl.ui.components.BottomBar
+import gr.unipi.thesis.dimstyl.ui.components.TopBar
+import gr.unipi.thesis.dimstyl.ui.components.modalDrawerSheet.ModalDrawerSheet
+import gr.unipi.thesis.dimstyl.ui.navigation.AppNavHost
+import gr.unipi.thesis.dimstyl.ui.navigation.NavRoute
+import gr.unipi.thesis.dimstyl.ui.theme.BodyColor
+import gr.unipi.thesis.dimstyl.ui.theme.LogoutColor
+import kotlinx.coroutines.launch
+
 @Composable
 fun MainScreen(navController: NavController = rememberNavController(), viewModel: MainViewModel) {
+    val scope = rememberCoroutineScope()
+    val mainState by viewModel.state.collectAsState()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(
+                state = mainState,
+                onNavigate = { route, title ->
+                    viewModel.setCurrentNavRoute(route)
+                    viewModel.setTopBarTitle(title)
+                    navController.navigate(NavRoute.getRoute(route))
+                    scope.launch { drawerState.close() }
+                },
+                onLogout = { viewModel.openLogoutDialog() }
+            )
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                TopBar(mainState.topBarTitle) { scope.launch { drawerState.open() } }
+            },
+            bottomBar = {
+                BottomBar(mainState) { route, title ->
+                    viewModel.setCurrentNavRoute(route)
+                    viewModel.setTopBarTitle(title)
+                    navController.navigate(NavRoute.getRoute(route))
+                }
+            },
+            containerColor = BodyColor
+        ) { innerPadding ->
+            AppNavHost(navController, innerPadding)
+
+            if (mainState.openLogoutDialog) {
+                AlertDialog(
+                    title = "Logout",
+                    text = "Are you sure you want to logout?",
+                    confirmButtonText = "Yes, Logout",
+                    dismissButtonText = "No, Cancel",
+                    icon = Icons.AutoMirrored.Default.ExitToApp,
+                    iconContentColor = LogoutColor,
+                    onDismiss = { viewModel.closeLogoutDialog() },
+                    onConfirm = {
+                        // TODO: Implement logout logic
+                        viewModel.closeLogoutDialog()
+                    },
+                )
+            }
+        }
+    }
 }
