16cba835a47619265b9bfc706844871eb44043f2
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-13 13:11:51 +0300

    Update Theme.kt (DietitianHub-Android)

--- DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/theme/Theme.kt
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package gr.unipi.thesis.dimstyl.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
