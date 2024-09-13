98ca0219e2b3472753e67b1ea1de10d52d6c9152
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-13 14:59:23 +0300

    TH-75 Refactor MainActivity to integrate MainScreen with MainViewModel (DietitianHub-Android)
    - Update `MainActivity` to include a `Surface` with `fillMaxSize()` modifier, ensuring full-screen usage.
    - Integrate `MainScreen` within `MainActivity`, passing an instance of `MainViewModel` to manage the screen's state.

--- DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/MainActivity.kt
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/MainActivity.kt
@@ -3,6 +3,12 @@ package gr.unipi.thesis.dimstyl
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import gr.unipi.thesis.dimstyl.ui.screens.main.MainScreen
+import gr.unipi.thesis.dimstyl.ui.screens.main.MainViewModel
 import gr.unipi.thesis.dimstyl.ui.theme.DietitianHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,6 +17,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DietitianHubTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    val mainViewModel: MainViewModel = viewModel()
+                    MainScreen(viewModel = mainViewModel)
+                }
             }
         }
     }
