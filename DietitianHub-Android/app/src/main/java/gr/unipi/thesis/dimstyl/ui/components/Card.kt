bd1ce4736912f1f0823507d0c6402e581d1b3d20
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-16 14:46:52 +0300

    TH-36 Create Card composable (DietitianHub-Android)

---
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/components/Card.kt
@@ -0,0 +1,40 @@
+package gr.unipi.thesis.dimstyl.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import gr.unipi.thesis.dimstyl.ui.theme.CardContainerColor
+import gr.unipi.thesis.dimstyl.ui.theme.CardTitleColor
+
+@Composable
+fun Card(title: String, createdAt: String, createdAtColor: Color) {
+    androidx.compose.material3.Card(
+        modifier = Modifier.size(width = 300.dp, height = 150.dp),
+        colors = CardDefaults.cardColors(containerColor = CardContainerColor),
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 6.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = title,
+                textAlign = TextAlign.Center,
+                color = CardTitleColor
+            )
+            Text(text = createdAt, color = createdAtColor)
+        }
+    }
+}
