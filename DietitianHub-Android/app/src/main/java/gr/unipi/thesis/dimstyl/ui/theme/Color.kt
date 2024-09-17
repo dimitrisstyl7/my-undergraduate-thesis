827f5cdccf50bff101b8b3552edf452adbf55f2d
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-18 00:53:26 +0300

    TH-37 Add download icon, refactor color palette and content types (DietitianHub-Android)
    - Add new `baseline_download_24.xml` icon to `res/drawable` for download button.
    - Refactor `Color.kt` by introducing new color constants for UI components:
      - Introduce `DownloadButtonColor`, `TableHeaderColor`, `TableCellColor`, and `TableTextColor`.
    - Extend `ContentType` enum to include new entries for diet plans table:
      - `DIET_PLANS_TABLE_HEADER` and `DIET_PLANS_TABLE_BODY`.

--- DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/theme/Color.kt
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/theme/Color.kt
@@ -15,15 +15,25 @@ val TopBarColor = Color(0xFFFFC40E)
 val BottomBarColor = Color(0xFF113537)
 val BodyColor = Color(0xFFEBEFF2)
 val LogoutColor = Color(0xFFEC3624)
+
 val EditButtonColor = Color(0xFF71B6F9)
 val SaveButtonColor = Color(0xFF10C469)
 val CancelButtonColor = Color(0xFFFF5B5B)
+val DownloadButtonColor = Color(0xFF71B6F9)
+
 val InputTextColor = Color(0xFF6C747C)
+
 val CardContainerColor = Color.White
+
 val AnnouncementTitleColor = Color(0xFF343A40)
 val AnnouncementSectionTitleColor = Color.White
 val AnnouncementsFirstSectionBackgroundColor = Color(0xFF5E68BA)
 val AnnouncementsSecondSectionBackgroundColor = Color(0xFFFB89C9)
 val AnnouncementsThirdSectionBackgroundColor = Color(0xFF76B5F6)
+
 val ArticleTitleColor = Color(0xFF333A45)
 val ArticleCreatedAtColor = Color(0xFF98A5AC)
+
+var TableHeaderColor = Color.White
+val TableCellColor = Color(0xFFF3F4F5)
+val TableTextColor = Color(0xFF6C747C)
