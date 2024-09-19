b0a557e6fbc79b3e592ea49c024fe6b3758ea393
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-19 23:09:09 +0300

    Create DatePickerDialog.kt (DietitianHub-Android)

---
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/components/dialogs/DatePickerDialog.kt
@@ -0,0 +1,29 @@
+package gr.unipi.thesis.dimstyl.ui.components.dialogs
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerDialog(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    androidx.compose.material3.DatePickerDialog(
+        onDismissRequest = onDismiss,
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onConfirm) {
+                Text("OK")
+            }
+        },
+        content = { content() }
+    )
+}
