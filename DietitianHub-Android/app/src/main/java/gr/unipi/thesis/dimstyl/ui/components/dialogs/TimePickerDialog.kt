fe527d73413a03b1e83914b4e880b4a5d3a87d63
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-19 23:38:49 +0300

    TH-79 Create TimePickerDialog and FloatingActionButton components (DietitianHub-Android)

---
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/components/dialogs/TimePickerDialog.kt
@@ -0,0 +1,27 @@
+package gr.unipi.thesis.dimstyl.ui.components.dialogs
+
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TimePickerDialog(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    androidx.compose.material3.AlertDialog(
+        onDismissRequest = onDismiss,
+        dismissButton = {
+            TextButton(onClick = { onDismiss() }) {
+                Text("Dismiss")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = { onConfirm() }) {
+                Text("OK")
+            }
+        },
+        text = { content() }
+    )
+}
