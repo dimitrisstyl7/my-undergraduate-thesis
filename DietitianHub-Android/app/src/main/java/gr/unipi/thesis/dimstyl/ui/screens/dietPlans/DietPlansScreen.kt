ea9ec96b7d17b999ebb228c0d3a8a9c7f9b56e0b
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-19 23:27:43 +0300

    Update DietPlansScreen (DietitianHub-Android)
    - Integrate `DietPlansViewModel` to provide state management and loading indicator.
    - Display a `CircularProgressIndicator` while data is loading.
    - Update table header and cells to use `IconTableCell` for download actions.
    - Add a download icon to `IconTableCell` and provide a TODO for implementing click action.
    - Adjust table header text capitalization for consistency.

--- DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/screens/dietPlans/DietPlansScreen.kt
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/screens/dietPlans/DietPlansScreen.kt
@@ -7,66 +7,90 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import gr.unipi.thesis.dimstyl.R
+import gr.unipi.thesis.dimstyl.ui.components.CircularProgressIndicator
+import gr.unipi.thesis.dimstyl.ui.components.IconTableCell
+import gr.unipi.thesis.dimstyl.ui.components.TableCell
 import gr.unipi.thesis.dimstyl.ui.helpers.ContentType
-import gr.unipi.thesis.dimstyl.ui.screens.dietPlans.components.DownloadIconTableCell
-import gr.unipi.thesis.dimstyl.ui.screens.dietPlans.components.TableCell
 import gr.unipi.thesis.dimstyl.ui.theme.BodyColor
 import gr.unipi.thesis.dimstyl.ui.theme.LeftBarColor
+import gr.unipi.thesis.dimstyl.ui.theme.PrimaryColor
 import gr.unipi.thesis.dimstyl.ui.theme.TableHeaderColor
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun DietPlansScreen() {
-    LazyColumn(
-        Modifier
-            .fillMaxSize()
-            .background(BodyColor)
-    ) {
-        stickyHeader(contentType = ContentType.DIET_PLANS_TABLE_HEADER) {
-            Row {
-                TableCell(
-                    weight = 0.2f,
-                    text = "#",
-                    fontWeight = FontWeight.Bold,
-                    textColor = TableHeaderColor,
-                    cellColor = LeftBarColor
-                )
+fun DietPlansScreen(viewModel: DietPlansViewModel = viewModel()) {
+    val state by viewModel.state.collectAsState()
 
-                TableCell(
-                    weight = 0.5f,
-                    text = "Created on",
-                    fontWeight = FontWeight.Bold,
-                    textColor = TableHeaderColor,
-                    cellColor = LeftBarColor
-                )
+    if (state.isLoading) {
+        CircularProgressIndicator()
+    } else {
+        LazyColumn(
+            Modifier
+                .fillMaxSize()
+                .background(BodyColor)
+        ) {
+            stickyHeader(contentType = ContentType.DIET_PLANS_TABLE_HEADER) {
+                Row {
+                    TableCell(
+                        weight = 0.2f,
+                        text = "#",
+                        fontWeight = FontWeight.Bold,
+                        textColor = TableHeaderColor,
+                        cellColor = LeftBarColor
+                    )
 
-                TableCell(
-                    weight = 0.3f,
-                    text = "Actions",
-                    fontWeight = FontWeight.Bold,
-                    textColor = TableHeaderColor,
-                    cellColor = LeftBarColor
-                )
+                    TableCell(
+                        weight = 0.5f,
+                        text = "Created On",
+                        fontWeight = FontWeight.Bold,
+                        textColor = TableHeaderColor,
+                        cellColor = LeftBarColor
+                    )
+
+                    TableCell(
+                        weight = 0.3f,
+                        text = "Actions",
+                        fontWeight = FontWeight.Bold,
+                        textColor = TableHeaderColor,
+                        cellColor = LeftBarColor
+                    )
+                }
             }
-        }
 
-        itemsIndexed(
-            items = dietPlans,
-            contentType = { _, _ -> ContentType.DIET_PLANS_TABLE_BODY }) { index, dietPlan ->
-            Row {
-                TableCell(
-                    weight = 0.2f,
-                    text = (index + 1).toString(),
-                    fontWeight = FontWeight.Bold
-                )
-                TableCell(weight = 0.5f, text = dietPlan.createdOn)
-                DownloadIconTableCell(weight = 0.3f)
+            itemsIndexed(
+                items = state.dietPlans,
+                contentType = { _, _ -> ContentType.DIET_PLANS_TABLE_BODY }) { index, dietPlan ->
+                Row {
+                    TableCell(
+                        weight = 0.2f,
+                        text = (index + 1).toString(),
+                        fontWeight = FontWeight.Bold
+                    )
+                    TableCell(weight = 0.5f, text = dietPlan.createdOn)
+                    IconTableCell(
+                        weight = 0.3f,
+                        buttonColor = PrimaryColor,
+                        icon = {
+                            Icon(
+                                painter = painterResource(R.drawable.baseline_download_24),
+                                contentDescription = "Download the diet plan created on ${dietPlan.createdOn}"
+                            )
+                        },
+                        onClick = { /* TODO: Download the diet plan */ }
+                    )
+                }
+                HorizontalDivider()
             }
-            HorizontalDivider()
         }
     }
 }
