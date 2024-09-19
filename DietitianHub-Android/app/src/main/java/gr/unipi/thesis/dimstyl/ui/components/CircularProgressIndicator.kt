3e4d838e614b2e7b8670140efba806a8c08e8b6d
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-19 23:07:28 +0300

    Create CircularProgressIndicator.kt (DietitianHub-Android)

---
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/components/CircularProgressIndicator.kt
@@ -0,0 +1,24 @@
+package gr.unipi.thesis.dimstyl.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import gr.unipi.thesis.dimstyl.ui.theme.PrimaryColor
+
+@Composable
+fun CircularProgressIndicator() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        androidx.compose.material3.CircularProgressIndicator(
+            color = PrimaryColor,
+            trackColor = Color.Transparent
+        )
+    }
+}
