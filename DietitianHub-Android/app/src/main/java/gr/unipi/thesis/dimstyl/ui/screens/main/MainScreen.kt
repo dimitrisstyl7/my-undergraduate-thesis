27d8249d43736528b6b0a42431c2bd7765f208b1
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-19 23:19:27 +0300

    Enhance AlertDialog in MainScreen for improved clarity and style (DietitianHub-Android)
    - Update AlertDialog text to emphasize re-login requirement after logout.
    - Change dialog icon to a rounded style for a modern look.
    - Switch icon color to DangerColor, as LogoutColor not exists.
    - Center text alignment in AlertDialog for improved readability.

--- DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/screens/main/MainScreen.kt
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/screens/main/MainScreen.kt
@@ -1,25 +1,27 @@
 package gr.unipi.thesis.dimstyl.ui.screens.main
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.automirrored.rounded.ExitToApp
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import gr.unipi.thesis.dimstyl.ui.components.AlertDialog
 import gr.unipi.thesis.dimstyl.ui.components.BottomBar
 import gr.unipi.thesis.dimstyl.ui.components.TopBar
+import gr.unipi.thesis.dimstyl.ui.components.dialogs.AlertDialog
 import gr.unipi.thesis.dimstyl.ui.components.modalDrawerSheet.ModalDrawerSheet
 import gr.unipi.thesis.dimstyl.ui.navigation.AppNavHost
 import gr.unipi.thesis.dimstyl.ui.navigation.NavRoute
 import gr.unipi.thesis.dimstyl.ui.theme.BodyColor
-import gr.unipi.thesis.dimstyl.ui.theme.LogoutColor
+import gr.unipi.thesis.dimstyl.ui.theme.DangerColor
 import kotlinx.coroutines.launch
 
 @Composable
@@ -61,11 +63,16 @@ fun MainScreen(navController: NavController = rememberNavController(), viewModel
             if (mainState.showLogoutDialog) {
                 AlertDialog(
                     title = "Logout",
-                    text = "Are you sure you want to logout?",
+                    text = {
+                        Text(
+                            text = "Are you sure you want to log out? You will need to log in again next time.",
+                            textAlign = TextAlign.Center
+                        )
+                    },
                     confirmButtonText = "Yes, Logout",
                     dismissButtonText = "No, Cancel",
-                    icon = Icons.AutoMirrored.Default.ExitToApp,
-                    iconContentColor = LogoutColor,
+                    icon = Icons.AutoMirrored.Rounded.ExitToApp,
+                    iconContentColor = DangerColor,
                     onDismiss = { viewModel.showLogoutDialog(false) },
                     onConfirm = {
                         // TODO: Implement logout logic
