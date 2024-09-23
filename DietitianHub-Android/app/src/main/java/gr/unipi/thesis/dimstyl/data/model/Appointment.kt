bfac94a4b0ec0a312094a09aa6d830b2da8434f6
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-23 19:30:05 +0300

    Add AppointmentStatus enum and update Appointment model (DietitianHub-Android)
    - Add `AppointmentStatus` enum to define appointment statuses (PENDING, SCHEDULED, COMPLETED, CANCELLED, DECLINED).
    - Rename `scheduledAt` to `start` in `Appointment` model to match with backend implementation.
    - Add `status` field to `Appointment` model to track appointment status.

--- DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/data/model/Appointment.kt
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/data/model/Appointment.kt
@@ -1,3 +1,3 @@
 package gr.unipi.thesis.dimstyl.data.model
 
-data class Appointment(val id: Int, val scheduledAt: String)
+data class Appointment(val id: Int, val start: String, val status: AppointmentStatus)
---
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/data/model/AppointmentStatus.kt
@@ -0,0 +1,7 @@
+package gr.unipi.thesis.dimstyl.data.model
+
+enum class AppointmentStatus {
+
+    PENDING, SCHEDULED, COMPLETED, CANCELLED, DECLINED
+
+}
