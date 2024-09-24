0f885b292772de8bc0d3370fdcba37919f2a8208
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-24 22:52:06 +0300

    Refactor HorizontalDivider component for flexibility (DietitianHub-Android)
    - Add parameters for `paddingValues`, `thickness`, and `color` to make the divider customizable.

--- DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/components/modalDrawerSheet/HorizontalDivider.kt
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/components/modalDrawerSheet/HorizontalDivider.kt
@@ -1,19 +1,22 @@
 package gr.unipi.thesis.dimstyl.ui.components.modalDrawerSheet
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun HorizontalDivider() {
+fun HorizontalDivider(
+    paddingValues: PaddingValues = PaddingValues(vertical = 10.dp, horizontal = 18.dp),
+    thickness: Dp = 2.dp,
+    color: Color = Color.White
+) {
     androidx.compose.material3.HorizontalDivider(
-        modifier = Modifier.padding(
-            vertical = 10.dp,
-            horizontal = 18.dp
-        ),
-        thickness = 2.dp,
-        color = Color.White
+        modifier = Modifier.padding(paddingValues),
+        thickness = thickness,
+        color = color
     )
 }
