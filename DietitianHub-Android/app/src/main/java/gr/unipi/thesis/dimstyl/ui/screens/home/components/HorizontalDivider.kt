9796f883621203d22b4f2c6308f876fecfd83a46
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-24 23:07:23 +0300

    TH-80 Implement HomeState, HomeViewModel and Refactor HomeScreen (DietitianHub-Android)
    - Introduce `HorizontalDivider` composable to visually separate greeting message from other sections within the UI.
    - Create `HomeState` data class to encapsulate home screen data management, including articles, announcements, and appointment data.
    - Implement `HomeViewModel` to handle data fetching and state management for the home screen, including initialization of header cells for appointments.
    - Update `HomeScreen` to utilize `HomeViewModel`, displaying personalized articles, announcements, and upcoming appointments dynamically.
    - Enhance `ContentType` enum to include new content types for better categorization of UI components.

---
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/screens/home/components/HorizontalDivider.kt
@@ -0,0 +1,34 @@
+package gr.unipi.thesis.dimstyl.ui.screens.home.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import gr.unipi.thesis.dimstyl.ui.theme.LeftBarColor
+import gr.unipi.thesis.dimstyl.ui.theme.TopBarColor
+
+@Composable
+fun HorizontalDivider(
+    maxWidth: Float = 0.75f,
+    thickness: Dp = 3.dp,
+    colors: List<Color> = listOf(LeftBarColor, TopBarColor)
+) {
+    Canvas(
+        Modifier
+            .height(thickness)
+            .fillMaxWidth(maxWidth)
+    ) {
+        drawLine(
+            brush = Brush.linearGradient(colors),
+            start = Offset(0f, size.height / 2),
+            end = Offset(size.width, size.height / 2),
+            strokeWidth = (thickness).toPx()
+        )
+    }
+}
