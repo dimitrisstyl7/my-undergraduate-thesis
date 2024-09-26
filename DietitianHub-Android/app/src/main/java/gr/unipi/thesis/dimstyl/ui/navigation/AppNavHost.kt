9c5c8b6eda22e84a8772e50615ec3e40b4269e6c
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-27 02:34:44 +0300

    TH-81 Refactor `AppNavHost` (DietitianHub-Android)
    - Integrate `MainViewModel` and `MainState` into `AppNavHost` for improved navigation handling based on login status.
    - Update start destination to `Landing` and implement login status logic in `LandingScreen`.
    - Add navigation logic to `HomeScreen` and `LoginScreen` to manage user authentication flow and route management.

--- DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/navigation/AppNavHost.kt
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/navigation/AppNavHost.kt
@@ -8,26 +8,51 @@ import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import gr.unipi.thesis.dimstyl.ui.helpers.LoginStatus
 import gr.unipi.thesis.dimstyl.ui.screens.announcements.AnnouncementsScreen
 import gr.unipi.thesis.dimstyl.ui.screens.appointments.AppointmentsScreen
 import gr.unipi.thesis.dimstyl.ui.screens.articles.ArticlesScreen
-import gr.unipi.thesis.dimstyl.ui.screens.auth.LoginScreen
 import gr.unipi.thesis.dimstyl.ui.screens.dietPlans.DietPlansScreen
 import gr.unipi.thesis.dimstyl.ui.screens.home.HomeScreen
+import gr.unipi.thesis.dimstyl.ui.screens.landing.LandingScreen
+import gr.unipi.thesis.dimstyl.ui.screens.login.LoginScreen
+import gr.unipi.thesis.dimstyl.ui.screens.main.MainState
+import gr.unipi.thesis.dimstyl.ui.screens.main.MainViewModel
 import gr.unipi.thesis.dimstyl.ui.screens.profile.ProfileScreen
 
 @Composable
 fun AppNavHost(
     navController: NavController,
+    viewModel: MainViewModel,
+    mainState: MainState,
     backHandler: @Composable () -> Unit,
     innerPadding: PaddingValues
 ) {
     NavHost(
         navController = navController as NavHostController,
-        startDestination = Home,
+        startDestination = Landing,
         modifier = Modifier.padding(innerPadding)
     ) {
-        composable<Home> { HomeScreen() }
+        composable<Landing> {
+            LandingScreen(
+                loginStatus = mainState.loginStatus,
+                onLoginStatusResolved = {
+                    viewModel.setCurrentNavRoute(NavRoute.HOME)
+                    navController.navigate(Home) { popUpTo(Landing) { inclusive = true } }
+                }
+            )
+        }
+
+        composable<Home> {
+            HomeScreen(
+                loginStatus = mainState.loginStatus,
+                onNavigateToLoginScreen = {
+                    viewModel.setLoginStatus(LoginStatus.LOGGED_OUT)
+                    viewModel.setCurrentNavRoute(NavRoute.LOGIN)
+                    navController.navigate(Login) { popUpTo(Home) { inclusive = true } }
+                }
+            )
+        }
 
         composable<Profile> { ProfileScreen() }
 
@@ -38,6 +63,14 @@ fun AppNavHost(
         composable<Announcements> { AnnouncementsScreen() }
 
         composable<DietPlans> { DietPlansScreen() }
+
+        composable<Login> {
+            LoginScreen {
+                viewModel.setLoginStatus(LoginStatus.LOGGED_IN)
+                viewModel.setCurrentNavRoute(NavRoute.HOME)
+                navController.navigate(Home) { popUpTo(Login) { inclusive = true } }
+            }
+        }
     }
 
     backHandler()
