b0f12cfe8482279a6b7563aaf192f667d7b3fb63
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-27 16:42:10 +0300

    `LoginPageFilter` to extend `OncePerRequestFilter` and enhance request filtering (DietitianHub-Backend)
    - Update `LoginPageFilter` to extend `OncePerRequestFilter` instead of `GenericFilterBean` for better control over per-request filtering logic.
    - Retain logic to redirect authenticated users from the `/auth/login` page to `/index`.
    - Add `@NonNullApi` annotation for enforcing non-null parameter handling across the filter.

--- DietitianHub-Backend/src/main/java/gr/unipi/thesis/dimstyl/security/LoginPageFilter.java
+++ DietitianHub-Backend/src/main/java/gr/unipi/thesis/dimstyl/security/LoginPageFilter.java
@@ -1,32 +1,30 @@
 package gr.unipi.thesis.dimstyl.security;
 
+import io.micrometer.common.lang.NonNullApi;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class LoginPageFilter extends GenericFilterBean {
+@NonNullApi
+public class LoginPageFilter extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         if (isAuthenticated() && request.getRequestURI().equals("/auth/login")) {
             String encodedRedirectURL = response.encodeRedirectURL(request.getContextPath() + "/index");
             response.setStatus(HttpServletResponse.SC_TEMPORARY_REDIRECT);
             response.setHeader("Location", encodedRedirectURL);
         }
 
-        filterChain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(request, response);
     }
 
     private boolean isAuthenticated() {
