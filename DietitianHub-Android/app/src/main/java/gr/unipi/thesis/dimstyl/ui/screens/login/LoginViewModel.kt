ac0b746c222f63af739a974ce980db03c1314916
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-09-30 01:01:08 +0300

    Enhance login functionality in LoginState and LoginViewModel (DietitianHub-Android)
    - Expand LoginState data class by adding properties:
      - `processingLoginRequest` to track login request status.
      - `loginHasError` and `errorMessage` for error handling.
    
    - Refactor LoginViewModel to integrate `loginUseCase` for managing login operations.
    - Implement `login` method to handle login requests and update the state based on success or error.
    - Introduce `setProcessingLoginRequest` method to manage processing state during login.
    - Update error handling by capturing and storing error messages.

--- DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/screens/login/LoginViewModel.kt
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/ui/screens/login/LoginViewModel.kt
@@ -1,13 +1,32 @@
 package gr.unipi.thesis.dimstyl.ui.screens.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import gr.unipi.thesis.dimstyl.domain.usecase.LoginUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(private val loginUseCase: LoginUseCase) : ViewModel() {
 
-    val _state = MutableStateFlow(LoginState())
+    private val _state = MutableStateFlow(LoginState())
     val state = _state.asStateFlow()
+
+    fun login(onSuccessfulLogin: (String) -> Unit) {
+        viewModelScope.launch {
+            val result = loginUseCase.execute(state.value.username, state.value.password)
+
+            if (result.isSuccess) onSuccessfulLogin(result.getOrNull()!!)
+            else {
+                _state.value = _state.value.copy(loginHasError = true)
+                _state.value = _state.value.copy(
+                    errorMessage = result.exceptionOrNull()?.message!!
+                )
+            }
+
+            _state.value = _state.value.copy(processingLoginRequest = false)
+        }
+    }
 
     fun setUsername(username: String) {
         _state.value = _state.value.copy(username = username)
@@ -19,6 +38,10 @@ class LoginViewModel : ViewModel() {
 
     fun togglePasswordVisibility() {
         _state.value = _state.value.copy(isPasswordVisible = !_state.value.isPasswordVisible)
+    }
+
+    fun setProcessingLoginRequest(processing: Boolean) {
+        _state.value = _state.value.copy(processingLoginRequest = processing)
     }
 
 }
