489093298b86adeee7b64f14ead0d20889fee4bd
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-10-02 00:48:10 +0300

    Enhance Appointment Repository with Fetch and Create Functions (DietitianHub-Android)
    - Add support for fetching appointments (`fetchAppointments()`) and creating new appointments (`createAppointment()`) in the `AppointmentRepository`.
    - Update `AppointmentRepositoryImpl` to handle:
      - Fetching different types of appointments using the `AppointmentApiService`.
      - Creating appointments while handling conflict errors.

--- DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/data/repositories/AppointmentRepositoryImpl.kt
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/data/repositories/AppointmentRepositoryImpl.kt
@@ -1,10 +1,15 @@
 package gr.unipi.thesis.dimstyl.data.repositories
 
 import android.util.Log
+import gr.unipi.thesis.dimstyl.data.models.Appointment
+import gr.unipi.thesis.dimstyl.data.models.AppointmentsResponse
 import gr.unipi.thesis.dimstyl.data.sources.remote.services.AppointmentApiService
 import gr.unipi.thesis.dimstyl.data.utils.ErrorParser.extractErrorCode
 import gr.unipi.thesis.dimstyl.domain.repositories.AppointmentRepository
+import gr.unipi.thesis.dimstyl.utils.Constants.ErrorMessages.APPOINTMENT_ALREADY_EXISTS_ERROR_MESSAGE
 import gr.unipi.thesis.dimstyl.utils.Constants.ErrorMessages.CANCEL_APPOINTMENT_ERROR_MESSAGE
+import gr.unipi.thesis.dimstyl.utils.Constants.ErrorMessages.CREATE_APPOINTMENT_ERROR_MESSAGE
+import gr.unipi.thesis.dimstyl.utils.Constants.ErrorMessages.FETCH_APPOINTMENTS_ERROR_MESSAGE
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Response
@@ -14,6 +19,73 @@ class AppointmentRepositoryImpl(private val appointmentApiService: AppointmentAp
 
     companion object {
         private const val TAG = "AppointmentRepositoryImpl"
+    }
+
+    override suspend fun fetchAppointments(): Result<AppointmentsResponse> {
+        return withContext(Dispatchers.IO) {
+            val response: Response<AppointmentsResponse>
+
+            try {
+                response = appointmentApiService.fetchAppointments()
+            } catch (e: Exception) {
+                Log.e(TAG, "Failed to fetch appointments", e)
+                return@withContext Result.failure(Exception(FETCH_APPOINTMENTS_ERROR_MESSAGE))
+            }
+
+            if (response.isSuccessful) {
+                val body: AppointmentsResponse? = response.body()
+
+                when {
+                    body == null -> {
+                        Log.e(TAG, "Failed to fetch appointments: Response body is null")
+                        return@withContext Result.failure(Exception(FETCH_APPOINTMENTS_ERROR_MESSAGE))
+                    }
+
+                    else -> return@withContext Result.success(body)
+                }
+            } else {
+                Log.e(TAG, "Failed to fetch appointments: ${response.errorBody()?.string()}")
+                return@withContext Result.failure(Exception(FETCH_APPOINTMENTS_ERROR_MESSAGE))
+            }
+        }
+    }
+
+    override suspend fun createAppointment(appointment: Appointment): Result<Appointment> {
+        return withContext(Dispatchers.IO) {
+            val response: Response<Appointment>
+
+            try {
+                response = appointmentApiService.createAppointment(appointment)
+            } catch (e: Exception) {
+                Log.e(TAG, "Failed to create appointment", e)
+                return@withContext Result.failure(Exception(CREATE_APPOINTMENT_ERROR_MESSAGE))
+            }
+
+            if (response.isSuccessful) {
+                val body: Appointment? = response.body()
+
+                when {
+                    body == null -> {
+                        Log.e(TAG, "Failed to create appointment: Response body is null")
+                        return@withContext Result.failure(Exception(CREATE_APPOINTMENT_ERROR_MESSAGE))
+                    }
+
+                    else -> return@withContext Result.success(body)
+                }
+            } else {
+                val errorResponse = response.errorBody()?.string()
+                val errorCode = extractErrorCode(errorResponse)
+                var errorMessage = CREATE_APPOINTMENT_ERROR_MESSAGE
+
+                Log.e(TAG, "Failed to create appointment: $errorResponse")
+
+                if (errorCode != null && errorCode == 409) {
+                    errorMessage = APPOINTMENT_ALREADY_EXISTS_ERROR_MESSAGE
+                }
+
+                return@withContext Result.failure(Exception(errorMessage))
+            }
+        }
     }
 
     override suspend fun cancelAppointment(id: Int): Result<Unit> {
--- DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/domain/repositories/AppointmentRepository.kt
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/domain/repositories/AppointmentRepository.kt
@@ -1,7 +1,12 @@
 package gr.unipi.thesis.dimstyl.domain.repositories
 
+import gr.unipi.thesis.dimstyl.data.models.Appointment
+import gr.unipi.thesis.dimstyl.data.models.AppointmentsResponse
+
 interface AppointmentRepository {
 
+    suspend fun fetchAppointments(): Result<AppointmentsResponse>
+    suspend fun createAppointment(appointment: Appointment): Result<Appointment>
     suspend fun cancelAppointment(id: Int): Result<Unit>
 
 }
