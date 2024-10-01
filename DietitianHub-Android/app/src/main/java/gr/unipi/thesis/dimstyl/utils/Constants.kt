af9c2002788db3908dfe6ac5322bca92a3dc3214
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-10-01 15:05:07 +0300

    Refactor and extend Constants for error and success messages (DietitianHub-Android)
    - Move `Constants` to `gr.unipi.thesis.dimstyl.utils` package.
    - Simplify multiline error messages by using string concatenation for better readability.
    - Add new error messages for data fetching failures and appointment cancellation errors.
    - Add success message for successful appointment cancellation.

--- DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/utils/Constants.kt
+++ DietitianHub-Android/app/src/main/java/gr/unipi/thesis/dimstyl/utils/Constants.kt
@@ -5,15 +5,23 @@ class Constants {
     object ErrorMessages {
 
         const val DEFAULT_LOGIN_ERROR_MESSAGE =
-            """Something went wrong while logging you in.
-Please check your credentials and try again.
-If the problem persists, contact our support team for assistance."""
+            "Something went wrong while logging you in.\n" +
+                    "Please check your credentials and try again.\n" +
+                    "If the problem persists, contact our support team for assistance."
 
         const val TOKEN_RETRIEVAL_FAILED_ERROR_MESSAGE =
-            """Something went wrong while trying to retrieve your session. Please log in again to continue."""
+            "Something went wrong while trying to retrieve your session. Please log in again to continue."
 
         const val TOKEN_EXPIRED_ERROR_MESSAGE =
-            """Your session has expired. Please log in again to continue."""
+            "Your session has expired. Please log in again to continue."
+
+        const val FETCH_DATA_ERROR_MESSAGE =
+            "An error occurred while fetching data. Please try again later. " +
+                    "If the problem persists, contact our support team for assistance."
+
+        const val DEFAULT_CANCEL_APPOINTMENT_ERROR_MESSAGE =
+            "An error occurred while trying to cancel your appointment. Please try again later. " +
+                    "If the problem persists, contact our support team for assistance."
 
     }
 
@@ -21,6 +29,8 @@ If the problem persists, contact our support team for assistance."""
 
         const val MANUAL_LOGIN_SUCCESS_MESSAGE = "You have successfully logged in!"
         const val AUTO_LOGIN_SUCCESS_MESSAGE = "Welcome back!"
+        const val APPOINTMENT_CANCELED_SUCCESS_MESSAGE =
+            "Your appointment has been canceled successfully!"
 
     }
 
