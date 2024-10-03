74dd30db3e2a75145c3b01dd8e15df5ab5250a82
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-10-03 19:08:08 +0300

    Refactor DateTimeUtil: Simplify method access and visibility (DietitianHub-Backend)
    - Make `getFormattedDate(LocalDateTime)` and `getFormattedTime(LocalDateTime)` private to restrict their access.
    - Retain `getFormattedDateTime(LocalDateTime)` as the primary public method for formatting both date and time.

--- DietitianHub-Backend/src/main/java/gr/unipi/thesis/dimstyl/utilities/DateTimeUtil.java
+++ DietitianHub-Backend/src/main/java/gr/unipi/thesis/dimstyl/utilities/DateTimeUtil.java
@@ -18,18 +18,18 @@ public final class DateTimeUtil {
         return "%s %s %s".formatted(day, month, year);
     }
 
-    public static String getFormattedDate(LocalDateTime dateTime) {
-        return getFormattedDate(dateTime.toLocalDate());
-    }
-
-    public static String getFormattedTime(LocalDateTime dateTime) {
-        // Return the time in the format "hh:mm AM/PM"
-        return dateTime.format(DateTimeFormatter.ofPattern("hh:mm a", java.util.Locale.ENGLISH));
-    }
-
     public static String getFormattedDateTime(LocalDateTime dateTime) {
         // Return the date and time in the format "day month year, hh:mm AM/PM"
         return "%s, %s".formatted(getFormattedDate(dateTime), getFormattedTime(dateTime));
+    }
+
+    private static String getFormattedDate(LocalDateTime dateTime) {
+        return getFormattedDate(dateTime.toLocalDate());
+    }
+
+    private static String getFormattedTime(LocalDateTime dateTime) {
+        // Return the time in the format "hh:mm AM/PM"
+        return dateTime.format(DateTimeFormatter.ofPattern("hh:mm a", java.util.Locale.ENGLISH));
     }
 
 }
